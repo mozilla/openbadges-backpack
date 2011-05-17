6ee53dc32cc3ae347ca519b82795b2b00f1b78e1
Brian J Brennan <brianloveswords@gmail.com>
2011-05-17 01:08:02 -0400

    Throw-away implementation of logging in.

--- backpack/apps/users/urls.py
+++ backpack/apps/users/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('users.views',
     url(r'^$',          'login_or_continue'),
+    url(r'^login/?$',   'login'),
     url(r'^logout/?$',  'logout'),
 )
--- backpack/apps/users/views.py
+++ backpack/apps/users/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib import auth
 
@@ -7,8 +8,20 @@ def login_or_continue(request):
     if request.user.is_authenticated():
         return HttpResponse('authed')
     else:
-        return HttpResponse('not authed')
+        return render_to_response('login.html', {},
+                                  context_instance=RequestContext(request))
 
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect('/')
+
+def login(request):
+    email = request.POST.get('email', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=email, password=password)
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponse('logged in, chucklefuck')
+    else:
+        return HttpResponseRedirect('/')
+    
