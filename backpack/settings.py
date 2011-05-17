5167f1a3ea7d28c7860c9d4dd38a06693fa1ec88
Brian J Brennan <brianloveswords@gmail.com>
2011-05-17 00:14:17 -0400

    Set dummy index page.

--- backpack/settings.py
+++ backpack/settings.py
@@ -108,9 +108,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'backpack.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    path('templates'),
 )
 
 INSTALLED_APPS = (
--- backpack/urls.py
+++ backpack/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^include/', include('browsershim.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',          direct_to_template, {'template': 'index.html'}),
+    url(r'^include/',   include('browsershim.urls')),
+    url(r'^admin/',     include(admin.site.urls)),
 )
 
 media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
