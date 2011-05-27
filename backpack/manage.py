72da374b424c9b2cc72de8f84f53393071071f70
Brian J Brennan <brianloveswords@gmail.com>
2011-05-27 15:43:04 -0400

    Setup requirements file, start attempting to load from settings_local.py

--- backpack/manage.py
+++ backpack/manage.py
@@ -9,15 +9,18 @@ path = lambda *a: os.path.join(ROOT, *a)
 
 site.addsitedir(path('apps'))
 
-import imp
 try:
-    imp.find_module('settings') # Assumed to be in the same directory.
+    import settings_local as settings
 except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
-    sys.exit(1)
-
-import settings
+    try:
+        import settings 
+    except ImportError:
+        import sys
+        sys.stderr.write(
+            "Error: Tried importing 'settings_local.py' and 'settings.py' "
+            "but neither could be found (or they're throwin an ImportError)."
+            " Please come back and try later.")
+        raise
 
 if __name__ == "__main__":
     execute_manager(settings)
