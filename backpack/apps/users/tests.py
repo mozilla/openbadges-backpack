aae5dfbcf9f1c3d1d90663e21136a9104056e577
Brian J Brennan <brianloveswords@gmail.com>
2011-05-27 22:00:34 -0400

    Restructured tests.

--- backpack/apps/users/tests.py
+++ backpack/apps/users/tests.py
@@ -4,39 +4,52 @@ from django.contrib.auth.models import User
 from forms import UserCreationForm
 import views
 
-class CreateUser(TestCase):
+class UserTests(TestCase):
     def setUp(self):
+        self.u = {'email':'test@example.com', 'pass':'password'}
         self.factory = RequestFactory()
         self.client = Client()
     
     def test_create_valid_user(self):
-        test_email = 'test@example.com'
-        request = self.factory.post('/register', {'email': test_email,
-                                                  'password1':'rad', 'password2':'rad'})
+        request = self.factory.post('/register', {
+            'email': self.u['email'],
+            'password1': self.u['pass'],
+            'password2': self.u['pass'],
+        })
         response = views.register(request)
         try:
-            user = User.objects.get(username=test_email)
+            user = User.objects.get(username=self.u['email'])
         except User.DoesNotExist:
             user = None
         self.assertIsInstance(user, User, "User creation failed")
         self.assertEqual(response.status_code, 302, "Failed to redirect on successful creation")
     
     def test_non_matching_password(self):
-        test_email = 'test2@example.com'
-        request = self.factory.post('/register', {'email': test_email,
-                                                  'password1':'rad', 'password2':'tubular'})
+        request = self.factory.post('/register', {
+            'email': self.u['email'],
+            'password1': self.u['pass'],
+            'password2': 'tubular',
+        })
         response = views.register(request)
         try:
-            user = User.objects.get(username=test_email)
+            user = User.objects.get(username=self.u['email'])
         except User.DoesNotExist:
             user = None
         self.assertNotIsInstance(user, User, "User creation should have failed with non-matching password.")
         self.assertEqual(response.status_code, 200, "Should stay on same page when password validation fails")
 
     def test_new_users_need_to_activate(self):
-        email, password = ('test@example.com', 'password')
-        UserCreationForm({'email': email, 'password1': password, 'password2': password}).save()
-        logged_in = self.client.login(username=email, password=password)
+        UserCreationForm({
+            'email': self.u['email'],
+            'password1': self.u['pass'],
+            'password2': self.u['pass'],
+        }).save()
+        logged_in = self.client.login(username=self.u['email'], password=self.u['pass'])
         self.assertFalse(logged_in, "Should not be able to login without activating")
-
+        
+    def tearDown(self):
+        try:
+            User.objects.get(username=self.u['email']).delete()
+        except:
+            pass
         
