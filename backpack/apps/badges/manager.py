aa9b63ce096d566667f751e73a981bfc5812b287
Brian J Brennan <brianloveswords@gmail.com>
2011-05-31 12:44:31 -0400

    Whitespace

--- backpack/apps/badges/manager.py
+++ backpack/apps/badges/manager.py
@@ -1,9 +1,12 @@
 class BadgeManager(object):
     from connection import collection
+    
     def all(self):
         return self.filter()
+    
     def filter(self, **kwargs):
         return self.collection().find(kwargs)
+    
     def get(self, **kwargs):
         from models import Badge
         if 'pk' in kwargs:
