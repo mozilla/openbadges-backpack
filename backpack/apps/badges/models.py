2ea2eaf8f08f669b12fe8894563b1b4bdc035599
Brian J Brennan <brianloveswords@gmail.com>
2011-05-30 22:02:38 -0400

    Reorganizing and comments

--- backpack/apps/badges/models.py
+++ backpack/apps/badges/models.py
@@ -7,19 +7,6 @@ from manager import BadgeManager
 class Badge(object):
     from connection import collection
     objects = BadgeManager()
-    validators = {
-        'url':         [URLValidator()],
-        'name':        [LengthValidator(min=4, max=80)],
-        'description': [LengthValidator(min=4, max=140)],
-        'recipient':   [validate_email],
-        'evidence':    [RelativeURLValidator()],
-        'expires':     [validate_iso_date],
-        'icons':       [TypeValidator(dict), MinSizeValidator(1)],
-        'ttl':         [validate_integer],
-    }
-    
-    def __eq__(self, other):
-        return self.fields == other.fields
     
     def __init__(self, data):
         # required fields
@@ -34,6 +21,24 @@ class Badge(object):
         self.fields.update(data)
         self._errors = {}
 
+    def __eq__(self, other):
+        return self.fields == other.fields
+    
+    ######################
+    # Validation-related #
+    ######################
+    
+    validators = {
+        'url':         [URLValidator()],
+        'name':        [LengthValidator(min=4, max=80)],
+        'description': [LengthValidator(min=4, max=140)],
+        'recipient':   [validate_email],
+        'evidence':    [RelativeURLValidator()],
+        'expires':     [validate_iso_date],
+        'icons':       [TypeValidator(dict), MinSizeValidator(1)],
+        'ttl':         [validate_integer],
+    }
+    
     def full_clean(self):
         errors = {}
         try:
@@ -72,6 +77,10 @@ class Badge(object):
     def is_valid(self):
         return len(self.errors()) == 0
 
+    ############################
+    # Database-hitting actions #
+    ############################
+    
     def save(self):
         self.full_clean()
         objectid = self.collection().insert(self.fields)
@@ -83,3 +92,10 @@ class Badge(object):
     def delete(self):
         assert self.fields.get('_id', None) is not None, "Badge object can't be deleted because its _id attribute is set to None"
         self.collection().remove(self.fields['_id'])
+
+
+
+
+
+
+
