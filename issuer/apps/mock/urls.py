1cba6f6f2a30a821aa12da8cf0e63a61255cc05a
Brian J Brennan <brianloveswords@gmail.com>
2011-07-13 13:32:40 -0400

    removing mock issuer

--- issuer/apps/mock/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
-
-urlpatterns = patterns('mock.views',
-  url(r'^$',                                 'list_badges'),
-  url(r'^badge/(?P<badge_id>.+).badge$',     'badge_manifest'),
-  url(r'^issue/(?P<badge_id>.+).badge$',     'issue_badge'),
-)
