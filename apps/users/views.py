cbaa4dc0ab5f3a2fb4729974887825e2bf55dcd4
Brian J Brennan <brianloveswords@gmail.com>
2011-07-15 17:18:36 -0400

    Not using forms anymore.

--- apps/users/views.py
+++ apps/users/views.py
@@ -9,7 +9,6 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib import auth
 from django.contrib.auth.models import User
-from users.forms import UserCreationForm
 
 logger = logging.getLogger(__name__)
 
