fe123637d56b58d6bbe887efcd56cd630dedfda5
Brian J Brennan <brianloveswords@gmail.com>
2011-08-23 15:59:20 -0700

    Allow for custom messages to be sent to RegEx validators.

--- validator.js
+++ validator.js
@@ -26,36 +26,33 @@ var model_factory = function(){
   return model;
 };
 
-var ValidationError = function(type) { var e = new Error(type); e.type=type; return e; };
-
-// validators should return methods
-var RegEx = function(regex){
+var RegEx = function(regex, type){
   return function(input){
-    if (!regex.test(input)) throw new ValidationError('regexp');
+    if (!regex.test(input)) throw new Error(type || 'regexp');
   };
 };
 var ISODate = function(){
   return function(input){
     var parsed = new Date(Date.parse(input))
-    if (isNaN(parsed.getDate())) throw new ValidationError('isodate');
+    if (isNaN(parsed.getDate())) throw new Error('isodate');
   };
 };
 var Email = function(){
   // more or less RFC 2822
   var regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-  return RegEx(regex);
+  return RegEx(regex, 'email');
 };
 var MaxLength = function(len){
   return function(input) {
-    if (input.length > len) throw new ValidationError('length');
+    if (input.length > len) throw new Error('length');
   };
 };
 var URL = function(){
   var fq_url = /^(https?):\/\/[^\s\/$.?#].[^\s]*$/;
   var local_url = /^\/\S+$/;
   return function(input) {
-    try { RegEx(fq_url)(input) }
-    catch(e) { RegEx(local_url)(input) }
+    try { RegEx(fq_url, 'url')(input); }
+    catch(e) { RegEx(local_url, 'url')(input); }
   };
 };
 
@@ -75,7 +72,7 @@ Assertion.prototype.fields = {
   issued_at : optional(ISODate())
 };
 Badge.prototype.fields = {
-  version     : required(RegEx(/v?\d+\.\d+\.d+/)),
+  version     : required(RegEx(/^v?\d+\.\d+\.\d+$/)),
   name        : required(MaxLength(128)),
   description : required(MaxLength(128)),
   image       : required(URL()),
@@ -96,7 +93,28 @@ exports.validate = function(assertion){
 // Internal testing.
 var run_tests = function() {
   var vows = require('vows')
-    , assert = require('assert');
+    , assert = require('assert')
+    , fixtures = {
+        badge: function(){ return {
+          version: 'v0.5.0',
+          name: 'HTML5',
+          description: 'For rocking in the free world',
+          image: '/html5.png',
+          criteria: 'http://example.com/criteria.html'
+        }},
+        fixture: function(){ return {
+          recipient: 'bimmy@example.com',
+          evidence: '/bimmy-badge.json',
+          expires: '2040-08-13',
+          issued_at: '2011-08-23'
+        }},
+        issuer: function(){ return {
+          name: 'p2pu',
+          org: 'school of webcraft',
+          contact: 'admin@p2pu.org',
+          url: 'http://p2pu.org/schools/sow'
+        }}
+      };
   
   vows.describe('internal').addBatch({
     'A Badge prototype': {
@@ -105,13 +123,18 @@ var run_tests = function() {
         assert.include(topic, 'fields');
       }
     },
+    
     'A Badge instance': {
-      topic: (new Badge({what: 'lol'})),
+      topic: (new Badge(fixtures.badge())),
       'has `data`, `fields` and `errors` method': function(topic){
         assert.include(topic, 'data');
         // will be in prototype chain, not on object.
         assert.ok(topic.fields);
         assert.ok(topic.errors);
+      },
+      'has expected data': function(topic){
+        var supplied = fixtures.badge();
+        assert.equal(topic.data.name, supplied.name);
       },
       'missing all fields': {
         topic: new Badge(),
@@ -126,8 +149,21 @@ var run_tests = function() {
             assert.equal(errors[k], 'missing');
           })
         }
+      },
+      'with invalid `criteria` and `image`': {
+        'topic': function(){
+          var d = fixtures.badge();
+          d.criteria = 'not-a-good-url';
+          d.image = 'ftp://even-worse-url.com/';
+          return new Badge(d);
+        },
+        'will have two errors': function(topic){
+          var errors = topic.errors();
+          assert.equal(Object.keys(errors).length, 2);
+        }
       }
     },
+    
     'A Validator': {
       'of MaxLength': {
         topic: function(){ return function(len){ return MaxLength(len);} },
@@ -207,6 +243,10 @@ var run_tests = function() {
           assert.throws(function(){ topic()('@whatlolcom'); });
           assert.throws(function(){ topic()('no-at-at-alll'); });
           assert.throws(function(){ topic()('12902@@@.com'); });
+        },
+        'should throw `email` message': function(topic) {
+          try { topic()('b@ex') }
+          catch (e) { assert.equal(e.message, 'email'); }
         }
       }
     }
