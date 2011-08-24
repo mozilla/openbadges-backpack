f61fa46acf614f700ce83683e9833163eace3ab9
Brian J Brennan <brianloveswords@gmail.com>
2011-08-23 18:34:30 -0700

    More testing for the validator.

--- test/validator-test.js
+++ test/validator-test.js
@@ -2,10 +2,45 @@ var vows = require('vows')
   , assert = require('assert')
   , validate = require('../validator').validate
 
+var fixture = function(changes){
+  changes = changes || {}
+  var f = {
+    recipient: 'bimmy@example.com',
+    evidence: '/bimmy-badge.json',
+    expires: '2040-08-13',
+    issued_at: '2011-08-23',
+    badge: {
+      version: 'v0.5.0',
+      name: 'HTML5',
+      description: 'For rocking in the free world',
+      image: '/html5.png',
+      criteria: 'http://example.com/criteria.html',
+      issuer: {
+        name: 'p2pu',
+        org: 'school of webcraft',
+        contact: 'admin@p2pu.org',
+        url: 'http://p2pu.org/schools/sow'
+      }
+    }
+  }
+  function make_change(_base, _changes) {
+    Object.keys(_changes).forEach(function(k){
+      if (typeof _changes[k] === 'object') {
+        make_change(_base[k], _changes[k]);
+      } else {
+        _base[k] = _changes[k];
+      }
+    })
+  }
+  make_change(f, changes);
+  return f;
+};
+
+
 
 vows.describe('Badge Validator').addBatch({
-  'when validating anything': {
-    topic: function() { return validate({}) },
+  'When validating anything': {
+    topic: function() { return validate({}); },
     'we get a structure': {
       'with a `status` member': function(topic) {
         assert.include(topic, 'status')
@@ -14,5 +49,110 @@ vows.describe('Badge Validator').addBatch({
         assert.include(topic, 'error')
       }
     }
+  },
+  'When validating something invalid': {
+    topic: function() { return validate({}); },
+    'we get a bad status and errors': function(topic) {
+      assert.equal(topic.status, 'failure');
+      assert.ok(Object.keys(topic.error).length > 0);
+    }
+  },
+  'Invalid assertions': {
+    'with bad assertion.recipient': {
+      topic: [
+        fixture({recipient: 'lkajd'}),
+        fixture({recipient: 'alskj@asdk'}),
+        fixture({recipient: '@.com'}),
+        fixture({recipient: '909090'}),
+        fixture({recipient: '____!@'})
+      ],
+      'should fail with `email` errors': function(topic){
+        topic.forEach(function(invalid){
+          var result = validate(invalid);
+          assert.include(result.error, 'assertion.recipient');
+          assert.equal(result.error['assertion.recipient'], 'email');
+        })
+      }
+    },
+    'with bad assertion.evidence': {
+      topic: [
+        fixture({evidence: '-not-asdo'}),
+        fixture({evidence: 'ftp://bad-scheme'}),
+        fixture({evidence: '@.com:90/'})
+      ],
+      'should fail with `url` errors': function(topic){
+        topic.forEach(function(invalid){
+          var result = validate(invalid);
+          assert.include(result.error, 'assertion.evidence');
+          assert.equal(result.error['assertion.evidence'], 'url');
+        })
+      }
+    },
+    'with bad assertion.expires': {
+      topic: [
+        fixture({expires: 'oiajsd09gjas;oj09'}),
+        fixture({expires: 'foreever ago'}),
+        fixture({expires: '111111'}),
+        fixture({expires: '1314145531'}),
+        fixture({expires: '@.com:90/'})
+      ],
+      'should fail with `isodate` errors': function(topic){
+        topic.forEach(function(invalid){
+          var result = validate(invalid);
+          assert.include(result.error, 'assertion.expires');
+          assert.equal(result.error['assertion.expires'], 'isodate');
+        })
+      }
+    },
+    'with bad assertion.issued_at': {
+      topic: [
+        fixture({issued_at: 'oiajsd09gjas;oj09'}),
+        fixture({issued_at: 'foreever ago'}),
+        fixture({issued_at: '111111'}),
+        fixture({issued_at: '1314145531'}),
+        fixture({issued_at: '@.com:90/'})
+      ],
+      'should fail with `isodate` errors': function(topic){
+        topic.forEach(function(invalid){
+          var result = validate(invalid);
+          assert.include(result.error, 'assertion.issued_at');
+          assert.equal(result.error['assertion.issued_at'], 'isodate');
+        })
+      }
+    },
+    'with bad badge.version': {
+      topic: [
+        fixture({badge: {version: 'v100'}}),
+        fixture({badge: {version: '50'}}),
+        fixture({badge: {version: 'v10.1alpha'}})
+      ],
+      'should fail with `regex` errors': function(topic){
+        topic.forEach(function(invalid){
+          var result = validate(invalid);
+          assert.include(result.error, 'badge.version');
+          assert.equal(result.error['badge.version'], 'regex');
+        })
+      }
+    },
+    'with bad badge.name': {
+    },
+    'with bad badge.description': {
+    },
+    'with bad badge.image': {
+    },
+    'with bad badge.criteria': {
+    },
+    'with bad issuer.name': {
+    },
+    'with bad issuer.org': {
+    },
+    'with bad issuer.contact': {
+    },
+    'with bad issuer.url': {
+    },
   }
 }).export(module);
+
+console.dir(validate(fixture({version: 'v100'})));
+console.dir(validate(fixture({version: '50'})));
+console.dir(validate(fixture({version: 'v10.1alpha'})));
--- validator.js
+++ validator.js
@@ -1,3 +1,7 @@
+var field = function(required, validators){ return { required: required, validators: validators }; };
+var required = function() { return field(true, Array.prototype.slice.call(arguments)); };
+var optional = function() { return field(false, Array.prototype.slice.call(arguments)); };
+
 var model_factory = function(){
   var model = function(data){ this.data = data || {}; }
   model.prototype.fields = {}
@@ -9,8 +13,10 @@ var model_factory = function(){
     
     expectedFields.forEach(function(k){
       var errorType;
-      if (!provided[k] && fields[k].required) {
-        errorType = 'missing';
+      if (!provided[k]) {
+          if (fields[k].required) {
+            errorType = 'missing';
+          }
       } else {
           fields[k].validators.forEach(function(validator){
           try { validator(provided[k]); }
@@ -26,26 +32,31 @@ var model_factory = function(){
   return model;
 };
 
-var RegEx = function(regex, type){
-  return function(input){
-    if (!regex.test(input)) throw new Error(type || 'regexp');
-  };
-};
-var ISODate = function(){
-  return function(input){
-    var parsed = new Date(Date.parse(input))
-    if (isNaN(parsed.getDate())) throw new Error('isodate');
-  };
+
+var RegEx = function(regex, type) { return (
+  function(input){ if (!regex.test(input)) throw new Error(type || 'regex'); }
+)};
+var MaxLength = function(len) { return (
+  function(input) { if (input.length > len) throw new Error('length'); }
+)};
+var ISODate = function() {
+  function simpleISODate(input) {
+    var regex = /\d{4}-\d{2}-\d{2}/;
+    if (!regex.test(input)) return 0;
+        var pieces = input.split('-')
+    , year = parseInt(pieces[0], 10)
+    , month = parseInt(pieces[1], 10)
+    , day = parseInt(pieces[2], 10)
+    if (month > 12 || month < 1) return 0;
+    if (day > 31 || day < 1) return 0;
+    return (new Date(year, (month-1), day)).getTime();
+  }
+  return function(input){ if (!simpleISODate(input)) throw new Error('isodate');}
 };
 var Email = function(){
   // more or less RFC 2822
   var regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
   return RegEx(regex, 'email');
-};
-var MaxLength = function(len){
-  return function(input) {
-    if (input.length > len) throw new Error('length');
-  };
 };
 var URL = function(){
   var fq_url = /^(https?):\/\/[^\s\/$.?#].[^\s]*$/;
@@ -56,28 +67,25 @@ var URL = function(){
   };
 };
 
-var field = function(required, validators){ return { required: required, validators: validators }; };
-var required = function() { return field(true, Array.prototype.slice.call(arguments)); };
-var optional = function() { return field(false, Array.prototype.slice.call(arguments)); };
 
 var Assertion = model_factory();
 var Badge = model_factory();
 var Issuer = model_factory();
 
 Assertion.prototype.fields = {
-  recipient : required(Email()),
   //badge     : required(),
+  recipient : required(Email()),
   evidence  : optional(URL()),
   expires   : optional(ISODate()),
   issued_at : optional(ISODate())
 };
 Badge.prototype.fields = {
+  //issuer      : required(),
   version     : required(RegEx(/^v?\d+\.\d+\.\d+$/)),
   name        : required(MaxLength(128)),
   description : required(MaxLength(128)),
   image       : required(URL()),
-  criteria    : required(URL()),
-  //issuer      : required()
+  criteria    : required(URL())
 };
 Issuer.prototype.fields = {
   name    : required(MaxLength(128)),
@@ -86,12 +94,30 @@ Issuer.prototype.fields = {
   url     : optional(URL())
 };
 
-exports.validate = function(assertion){
-  return { status: 'okay', error: [] };
+var validate = function(assertionData){
+  var badgeData = assertionData.badge || {}
+    , issuerData = badgeData.issuer || {}
+    , assertion = new Assertion(assertionData)
+    , badge = new Badge(badgeData)
+    , issuer = new Issuer(issuerData)
+    , errors = {}
+  ;
+  function addToErrors(prefix, newErrors) {
+    Object.keys(newErrors).forEach(function(k){
+      errors[prefix + '.' + k] = newErrors[k];
+    });
+  }
+  addToErrors('assertion', assertion.errors());
+  addToErrors('badge', badge.errors());
+  addToErrors('issuer', issuer.errors());
+  return {
+    status: Object.keys(errors).length ? 'failure' : 'success',
+    error: errors
+  };
 };
 
 // Internal testing.
-var run_tests = function() {
+var runtests = function() {
   var vows = require('vows')
     , assert = require('assert')
     , fixtures = {
@@ -126,24 +152,24 @@ var run_tests = function() {
     
     'A Badge instance': {
       topic: (new Badge(fixtures.badge())),
-      'has `data`, `fields` and `errors` method': function(topic){
+      'has `data`, `fields` and `errors` method': function(topic) {
         assert.include(topic, 'data');
         // will be in prototype chain, not on object.
         assert.ok(topic.fields);
         assert.ok(topic.errors);
       },
-      'has expected data': function(topic){
+      'has expected data': function(topic) {
         var supplied = fixtures.badge();
         assert.equal(topic.data.name, supplied.name);
       },
       'missing all fields': {
         topic: new Badge(),
-        'will have all errors': function(topic){
+        'will have all errors': function(topic) {
           // currently 5 required fields.
           var errors = topic.errors()
           assert.equal(Object.keys(errors).length, 5);
         },
-        'will have all `missing` errors': function(topic){
+        'will have all `missing` errors': function(topic) {
           var errors = topic.errors();
           Object.keys(errors).forEach(function(k){
             assert.equal(errors[k], 'missing');
@@ -157,9 +183,15 @@ var run_tests = function() {
           d.image = 'ftp://even-worse-url.com/';
           return new Badge(d);
         },
-        'will have two errors': function(topic){
+        'will have two errors': function(topic) {
           var errors = topic.errors();
           assert.equal(Object.keys(errors).length, 2);
+        },
+        'will have `url` errors only': function(topic) {
+          var errors = topic.errors();
+          Object.keys(errors).forEach(function(k){
+            assert.equal(errors[k], 'url');
+          });
         }
       }
     },
@@ -252,4 +284,11 @@ var run_tests = function() {
     }
   }).run()
 }
-run_tests()
+
+if (typeof module !== 'undefined') {
+  if (!module.parent) {
+    runtests();
+  } else {
+    exports.validate = validate;
+  }
+}
