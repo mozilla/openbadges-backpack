18692a66efe9c112d5b657f292cb4d2a4ea9b6de
Brian J Brennan <brianloveswords@gmail.com>
2011-08-25 23:05:28 -0700

    Fixed test so it can run by `vows` instead of necessitating `node database-test.js`. Vows didn't like the tests being defined in a callback, nor did it like the topics setting `this.callback` in several nested callbacks.

--- test/database-test.js
+++ test/database-test.js
@@ -2,12 +2,11 @@ var vows = require('./setup').vows
   , assert = require('assert')
   , db = require('../database')
 
+// operations on the database will happen in order.
 var collection = db.collection('db_test');
 
 // clean slate before we begin
 collection.remove({}) 
-
-// poor man's fixture
 collection.insert([
   {name: 'brian'},
   {name: 'jeremy'},
@@ -31,94 +30,86 @@ collection.insert([
      }
    }
   },
-], function(err, docs){
-  
-  vows.describe('Database storage & retrieval').addBatch({
-    'A db interface': {
-      topic: collection,
-      'when inserting a document' : {
-        topic: function(collection) {
-          collection.insert({test: 'sup'}, this.callback);
-        },
-        'returns its structure': function(err, result){
-          var doc = result[0];
-          assert.ok(!err);
-          assert.equal(doc.test, 'sup');
-          assert.ok(doc['_id']);
-        },
+])
+
+vows.describe('Database storage & retrieval').addBatch({
+  'A db interface': {
+    topic: collection,
+    'when inserting a document' : {
+      topic: function(collection) {
+        collection.insert({test: 'sup'}, this.callback);
       },
-      'when finding documents': {
-        topic: function(collection){
-          collection.find({name: {'$exists': true} }, this.callback);
-        },
-        'gets the docs': function(err, docs){
-          assert.ok(docs.length >= 3);
-        }
+      'returns its structure': function(err, result){
+        var doc = result[0];
+        assert.ok(!err);
+        assert.equal(doc.test, 'sup');
+        assert.ok(doc['_id']);
       },
-      'when finding a specific document': {
-        topic: function(collection){
-          collection.find({name: 'brian'}, this.callback);
-        },
-        'gets the document': function(err, docs){
-          assert.equal(docs.length, 1);
-          assert.equal(docs[0].name, 'brian');
-        }
+    },
+    'when finding documents': {
+      topic: function(collection){
+        collection.find({name: {'$exists': true} }, this.callback);
       },
-      'when finding a complicated document': {
-        topic: function(collection){
-          var query = {recipient: 'bimmy@example.com', 'badge.issuer.name': 'p2pu', 'badge.issuer.org': 'school of webcraft'}
-          collection.find(query, this.callback);
-        },
-        'gets the document': function(err, docs){
-          assert.equal(docs.length, 1);
-        }
+      'gets the docs': function(err, docs){
+        assert.ok(docs.length >= 3);
+      }
+    },
+    'when finding a specific document': {
+      topic: function(collection){
+        collection.find({name: 'brian'}, this.callback);
       },
-      'when updating a doc': {
-        topic: function(collection){
-          var self = this;
-          collection.update({name: 'miko'}, {'$set': {name: 'ian'}}, function(err) {
-            if (err) throw err;
-            collection.find({name: 'ian'}, self.callback);
-          });
-        },
-        'the doc should change': function(err, docs) {
-          assert.equal(docs.length, 1);
-          assert.equal(docs[0].name, 'ian');
-        }
+      'gets the document': function(err, docs){
+        assert.equal(docs.length, 1);
+        assert.equal(docs[0].name, 'brian');
+      }
+    },
+    'when finding a complicated document': {
+      topic: function(collection){
+        var query = {recipient: 'bimmy@example.com', 'badge.issuer.name': 'p2pu', 'badge.issuer.org': 'school of webcraft'}
+        collection.find(query, this.callback);
       },
-      'when removing a doc': {
-        topic: function(collection) {
-          var self = this;
-          collection.remove({name: 'alex'}, function(err){
-            if (err) throw err;
-            collection.find({name: 'alex'}, self.callback)
-          })
-        },
-        'the doc be removed': function(err, docs) {
-          assert.equal(docs.length, 0);
-        }
+      'gets the document': function(err, docs){
+        assert.equal(docs.length, 1);
+      }
+    },
+    'when updating a doc': {
+      topic: function(collection){
+        collection.update({name: 'miko'}, {'$set': {name: 'ian'}}, function(err) {if (err) throw err;});
+        collection.find({name: 'ian'}, this.callback);
       },
-      'when upserting': {
-        topic: function(collection){
-          var self = this;
-          var selector = {plural: {crow: 'murder'}};
-          var newDoc = {plural: {crow: 'murder'}, is_this_awesome: 'totally'};
-          collection.upsert(selector, newDoc, function(err) {
-            if (err) throw err;
-            newDoc.is_this_awesome = 'yep';
-            collection.update(selector, newDoc, function(err) {
-              collection.find(selector, self.callback)
-            });
-          });
-        },
-        'there should only be one': function(err, docs) {
-          assert.equal(docs.length, 1);
-        },
-        'the value should be from the second update': function(err, docs) {
-          assert.equal(docs[0].is_this_awesome, 'yep');
-        }
+      'the doc should change': function(err, docs) {
+        assert.equal(docs.length, 1);
+        assert.equal(docs[0].name, 'ian');
+      }
+    },
+    'when removing a doc': {
+      topic: function(collection) {
+        collection.remove({name: 'alex'}, function(err){if (err) throw err;})
+        collection.find({name: 'alex'}, this.callback)
       },
-      teardown: function(){ db.connection.close(); }
-    }
-  }).export(module)
-})
+      'the doc be removed': function(err, docs) {
+        assert.equal(docs.length, 0);
+      }
+    },
+    'when upserting': {
+      topic: function(collection){
+        console.log('upserting');
+        var selector = {plural: {crow: 'murder'}};
+        var newDoc = {plural: {crow: 'murder'}, is_this_awesome: 'totally'};
+        collection.upsert(selector, newDoc, function(err) {
+          if (err) throw err;
+          newDoc.is_this_awesome = 'yep';
+        });
+        collection.upsert(selector, newDoc, function(err) {if (err) throw err;});
+        collection.find(selector, this.callback)
+      },
+      'there should only be one': function(err, docs) {
+        assert.equal(docs.length, 1);
+      },
+      'the value should be from the second upsert': function(err, docs) {
+        assert.equal(docs[0].is_this_awesome, 'yep');
+      }
+    },
+    teardown: function(){ db.connection.close(); }
+  }
+}).export(module)
