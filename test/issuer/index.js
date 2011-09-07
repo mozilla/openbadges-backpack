6749a910dc5da91ea4db1a1047b4e0a57017789c
Brian J Brennan <brianloveswords@gmail.com>
2011-09-07 02:02:27 -0400

    Making a unified test issuer that can serve simple requests and complex requests.

---
+++ test/issuer/index.js
@@ -0,0 +1,45 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , badge = require('../utils').fixture
+
+var app = express.createServer();
+var PORT = 10010;
+var HOST = '127.0.0.1';
+
+// simple server
+var makeServer = function(code, body, type) {
+  type = type || 'application/json';
+  return function(){
+    var server = http.createServer();
+    server.on('request', function(req, res){
+      res.statusCode = code;
+      res.setHeader('content-type', type);
+      res.end(body);
+    });
+    server.listen(++PORT, HOST);
+    server.port = PORT;
+    server.url = 'http://' + HOST + ':' + PORT + '/'
+    return server;
+  }
+}
+
+app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, "static")));
+app.get('/good.json', function(req, res){
+  res.send(badge());
+});
+
+exports.complex = app;
+exports.simple = {
+  good: makeServer(200, JSON.stringify(badge())),
+  bad: makeServer(200, JSON.stringify(badge({recipient: null}))),
+  reallyBad: makeServer(200, '()dj;1{}this will fail json;())}'),
+  extraBad: makeServer(200, '<html><body><h1>EY YO</h1></body></html>', 'text/html'),
+  dreadful: makeServer(500, 'totally messed up', 'text/plain')
+}
+
+if (!module.parent) {
+  console.log('listening on ' + PORT); app.listen(PORT);
+}
+
--- test/remote-test.js
+++ test/remote-test.js
@@ -1,6 +1,6 @@
 var vows = require('./setup')
   , assert = require('assert')
-  , issuer = require('./fake-issuer')
+  , issuer = require('./issuer')
   , remote = require('../remote')
   , database = require('../database')
   , configuration = require('../lib/configuration')
@@ -9,7 +9,7 @@ var vows = require('./setup')
 vows.describe('Issuing by remote assertion').addBatch({
   'Submitting a': {
     'good assertion': {
-      topic: issuer.goodAssertion(),
+      topic: issuer.simple.good(),
       'should get': {
         topic: function(server) { remote.assertion(server.url, this.callback) },
         '`status == success`': function(err, result){ assert.equal(err.status, 'success'); },
@@ -18,7 +18,7 @@ vows.describe('Issuing by remote assertion').addBatch({
       teardown: function(server){ server.close(); }
     },
     'bad assertion': {
-      topic: issuer.badAssertion(),
+      topic: issuer.simple.bad(),
       'should get': {
         topic: function(server) { remote.assertion(server.url, this.callback) },
         '`status == failure`': function(err, result){ assert.equal(err.status, 'failure') },
@@ -27,7 +27,7 @@ vows.describe('Issuing by remote assertion').addBatch({
       teardown: function(server){ server.close(); }
     },
     'really bad assertion (bad json)': {
-      topic: issuer.reallyBadAssertion(),
+      topic: issuer.simple.reallyBad(),
       'should get': {
         topic: function(server) { remote.assertion(server.url, this.callback) },
         '`status == failure`': function(err, result){ assert.equal(err.status, 'failure') },
@@ -36,7 +36,7 @@ vows.describe('Issuing by remote assertion').addBatch({
       teardown: function(server){ server.close(); }
     },
     'dreadful assertion (4xx or 5xx)': {
-      topic: issuer.dreadfulAssertion(),
+      topic: issuer.simple.dreadful(),
       'should get': {
         topic: function(server) { remote.assertion(server.url, this.callback) },
         '`status == failure`': function(err, result){ assert.equal(err.status, 'failure') },
