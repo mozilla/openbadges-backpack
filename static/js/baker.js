546961111a00fac934bb7ca962c72f9df06a45eb
Brian J Brennan <brianloveswords@gmail.com>
2011-09-07 07:29:50 -0400

    Made the badge creator a little more fancy.

--- static/js/baker.js
+++ static/js/baker.js
@@ -2,11 +2,25 @@
   var assertionField = $('#assertion');
   var resultSection = $('#result');
   var showBadge = function(badgeURL){
-    var badge = $('img');
-    resultSection.html('<img src=ballsack>');
-    console.log('should have been built');
+    var img = $('<img>').attr('src', badgeURL)
+      , header = $('<h3>').html('Vóila! Your badge, with assertion embedded.')
+      , link = $('<a>').attr('href', badgeURL)
+    link.append(img);
+    
+    resultSection.removeClass('failure').addClass('success');
+    resultSection.empty().append(header, link);
+    resultSection.animate({opacity: 1.0});
   }
   var showErrors = function(data) {
+    var header = $('<h3>').html('Oh no! There was a problem')
+      , description = $('<dl>')
+      , error = $('<dt>Error</dt><dd><code>' + data.error + '</code></dd>')
+      , reason = $('<dt>Reason</dt><dd>' + data.reason + '</dd>')
+    description.append(error, reason)
+    
+    resultSection.removeClass('success').addClass('failure');
+    resultSection.empty().append(header, description);
+    resultSection.animate({opacity: 1.0});
   }
   
   $('#bake-form').bind('submit', function(e){
@@ -14,14 +28,24 @@
       , badgeURL = '/baker?assertion='+assertionURL
     e.preventDefault();
     if (!assertionURL) return false;
+    
+    if (resultSection.css('opacity') !== 0) {
+      resultSection.animate({opacity:0});
+    }
+    
     jQuery.get(badgeURL, function(data){
       if (data.status === 'success') {
-        showBadge(badgeURL);
+        resultSection.queue('fx', function(next){
+          showBadge(badgeURL); next();
+        });
       }
       else {
-        showErrors(data);
+        resultSection.queue('fx', function(next){
+          showErrors(data); next();
+        });
       }
     }, 'json')
     return false;
   })
+  resultSection.css({opacity:0})
 })(jQuery);
