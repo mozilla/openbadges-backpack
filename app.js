5ce2f2cf26a419f915bfb01b157209ac7078b174
Brian J Brennan <brianloveswords@gmail.com>
2011-09-09 02:11:46 -0400

    Made an app runner.
    app.js now starts a server as a side effect. run.js spawns a child process
    that runs `node app.js` and watches it for termination. If the child
    dies or is killed, or if the parent process receives SIGHUP the runner
    spins up a new server.

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 // Configure & start express.
 var express = require('express')
   , ejs = require('ejs')
+  , fs = require('fs')
   , path = require('path')
   , middleware = require('./middleware')
   , controller = require('./controller')
   , logger = require('./lib/logging').logger
+  , configuration = require('./lib/configuration')
 ;
 
 // Create the app and set it up to use `ejs` templates which are easier to
 // maintain than the default `jade` templates.
 var app = express.createServer();
+app.logger = logger;
+app.config = configuration;
+
 app.set('view engine', 'ejs');
 
 // View helpers. `user` and `badges` are set so we can use them in `if`
@@ -39,5 +44,26 @@ app.get('/login',         controller.login);
 app.get('/baker',         controller.baker);
 app.get('/',              controller.manage);
 
+
+var start_server = function(app) {  
+  var port = app.config.get('port');
+  var pid = process.pid.toString();
+  var pidfile = path.join(app.config.get('var_path'), 'server.pid');
+
+  app.listen(port);
+  app.logger.info('pid: ' + pid);
+  app.logger.info('port: ' + port);
+  app.logger.info('READY PLAYER ONE')
+
+  fs.unlink(pidfile, function(){
+    fs.writeFile(pidfile, pid, function(err){
+      if (err) throw Error('could not make pidfile: ' + err)
+    });
+  })
+}
+
+start_server(app);
+
 exports.server = app;
 exports.logger = logger;
+exports.config = configuration;
--- run.js
+++ run.js
@@ -1,6 +1,36 @@
 #!/usr/bin/env node
+var spawn = require('child_process').spawn
+  , colors = require('colors')
 
-// FIXME: make port a configuration rather than hardcoded.
-var app = require('./app');
-app.server.listen(80);
-app.logger.info('READY PLAYER ONE');
+var running_server;
+var log = function() {
+  console.log("runner: ".magenta + Array.prototype.slice.call(arguments).join(' '));
+}
+
+var create_server = function(){
+  log('spawning new server');
+  var app = spawn('node', ['app.js']);
+  var fancypid = ('('+app.pid+') ').grey;
+
+  app.stdout.on('data', function(data){
+    process.stdout.write(fancypid);
+    process.stdout.write(data);
+  })
+  app.stderr.on('data', function(data){
+    process.stdout.write(fancypid);
+    process.stdout.write(data);
+  })
+  app.on('exit', function(code, sig) {
+    if (sig) log('caught signal '+sig)
+    if (code) log('exited with code '+code)
+    running_server = create_server();
+  })
+  return app;
+}
+process.on('SIGHUP', function () {
+  // should restart itself
+  running_server.kill();
+});
+
+log('parent pid:', process.pid);
+running_server = create_server();
