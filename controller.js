f899676aee49a0f145b70709d27d9c7f4353590d
Brian J Brennan <brianloveswords@gmail.com>
2011-09-09 05:22:39 -0400

    Add route and controller for test badge assertion.

--- controller.js
+++ controller.js
@@ -204,3 +204,27 @@ exports.login = function(req, res) {
     error: req.flash('login_error')
   });
 };
+
+exports.test_badge = function(req, res) {
+  var protocol = configuration.get('protocol') || 'http';
+  var port = configuration.get('external_port') || '';
+  
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    recipient: 'me@example.com',
+    evidence: '/test/evidence',
+    expires: '2040-08-13',
+    issued_on: '2011-08-23',
+    badge: {
+      version: 'v0.5.0',
+      name: 'Open Source Contributor',
+      description: 'For rocking in the free world',
+      image: '/images/test-badge.png',
+      criteria: '/test/criteria',
+      issuer: {
+        name: 'Open Badges',
+        origin: protocol + '://' + configuration.get('hostname') + (port? ':' + port : '')
+      }
+    }
+  }));
+}
