870405d0651b9fab4887346eb0900ad99d127f20
Brian J Brennan <brianloveswords@gmail.com>
2011-09-09 05:22:23 -0400

    Show validation errors properly in badge baker.

--- static/js/baker.js
+++ static/js/baker.js
@@ -11,12 +11,27 @@
     resultSection.empty().append(header, link);
     resultSection.animate({opacity: 1.0});
   }
+  var process_reason = function(reason) {
+    var html = '';
+    if (reason && typeof reason === "object") {
+      html += '<ul>';
+      for (field in reason) {
+        html += '<li><strong>'+ field +'</strong>: '+ reason[field] +'</li>';
+      }
+      html += '</ul>';
+    }
+    else { html = reason; }
+    return html;
+  }
+  
   var showErrors = function(data) {
     var header = $('<h3>').html('Oh no! There was a problem')
       , description = $('<dl>')
       , error = $('<dt>Error</dt><dd><code>' + data.error + '</code></dd>')
-      , reason = $('<dt>Reason</dt><dd>' + data.reason + '</dd>')
+      , reason = $('<dt>Reason</dt><dd>' + process_reason(data.reason) + '</dd>')
     description.append(error, reason)
+    
+    console.dir(data.reason);
     
     resultSection.removeClass('success').addClass('failure');
     resultSection.empty().append(header, description);
