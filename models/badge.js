677a950a593ad27748efdeb64f83bbabc7d7111a
Brian J Brennan <brianloveswords@gmail.com>
2011-09-10 07:33:39 -0400

    Validation test for model passes.

--- models/badge.js
+++ models/badge.js
@@ -9,11 +9,27 @@ var urlre = /(^(https?):\/\/[^\s\/$.?#].[^\s]*$)|(^\/\S+$)/
   , versionre = /^v?\d+\.\d+\.\d+$/
   
 var maxlen = function(len){
-  return function(v){ return v.length < len }
+  return function(v){ return (v||'').length < len }
 }
+
 var slashTrim = function(v){
   return v.replace(/\/*$/, '');
 }
+
+var isodate = function(){}
+isodate.set = function(input) {
+  console.dir(input);
+  var expression = /\d{4}-\d{2}-\d{2}/;
+  if (!expression.test(input)) return false;
+  var pieces = input.split('-')
+    , year = parseInt(pieces[0], 10)
+    , month = parseInt(pieces[1], 10)
+    , day = parseInt(pieces[2], 10)
+  if (month > 12 || month < 1) return false;
+  if (day > 31 || day < 1) return false;
+  return (new Date(year, (month-1), day)).getTime();
+};
+isodate.validate = function(v) { return v === null || v > 0; }
 
 var Schema = mongoose.Schema
   , ObjectId = Schema.ObjectId
@@ -26,8 +42,8 @@ var Badge = new Schema(
     }
   , recipient : { type: String, required: true, match: emailre, index: true }
   , evidence  : { type: String, match: urlre }
-  , expires   : Date
-  , issued_on : Date
+  , expires   : { type: String, set: isodate.set, validate: [isodate.validate, 'isodate'] }
+  , issued_on : { type: String, set: isodate.set, validate: [isodate.validate, 'isodate'] }
   , badge:
     { version     : { type: String, required: true, match: versionre }
     , name        : { type: String, required: true, validate: [maxlen(128), 'maxlen'] }
@@ -44,29 +60,24 @@ var Badge = new Schema(
   }
 )
 
-var BadgeModel = mongoose.model('Badge', Badge);
+var BadgeModel = module.exports = mongoose.model('Badge', Badge);
 
-var instance = new BadgeModel({
-  recipient: 'bimmy@example.com',
-  evidence: '/bimmy-badge.json',
-  expires: '2040-08-13',
-  issued_on: '2011-08-23',
-  badge: {
-    version: 'v0.5.0',
-    name: 'Open Source Contributor',
-    description: 'For rocking in the free world',
-    image: '/badge.png',
-    criteria: 'http://example.com/criteria.html',
-    issuer: {
-      origin: 'http://p2pu.org',
-      name: 'p2pu',
-      org: 'school of webcraft',
-      contact: 'admin@p2pu.org'
-    }
-  }
-});
-
-
-instance.validate(function(err){
-  console.dir(err);
-})
+// var instance = new BadgeModel({
+//   recipient: 'bimmy@example.com',
+//   evidence: '/bimmy-badge.json',
+//   expires: '2040-08-13',
+//   issued_on: '2011-08-23',
+//   badge: {
+//     version: 'v0.5.0',
+//     name: 'Open Source Contributor',
+//     description: 'For rocking in the free world',
+//     image: '/badge.png',
+//     criteria: 'http://example.com/criteria.html',
+//     issuer: {
+//       origin: 'http://p2pu.org',
+//       name: 'p2pu',
+//       org: 'school of webcraft',
+//       contact: 'admin@p2pu.org'
+//     }
+//   }
+// });
--- test/badge-model-test.js
+++ test/badge-model-test.js
@@ -21,57 +21,46 @@ var generateErrorTests = function(field, errType, badData) {
       changes = struct;
     }
     currentTest = tests['“' + badData[i] + '”'] = {}
-    currentTest['topic'] = fixture(changes);
-    currentTest['should fail with `' +  errType + '` error'] = function(topic) {
-      var result = validate(topic);
-      assert.include(result.errors, field);
-      assert.equal(result.errors[field], errType);
+    currentTest['topic'] = function(){
+      new Badge(fixture(changes)).validate(this.callback)
+    };
+    currentTest['should fail with `' +  errType + '` error'] = function(err, suc) {
+      assert.ok(err);
+      assert.includes(err.errors, field)
+      assert.equal(err.errors[field].type, errType);
     }
   }
   return tests;
 }
 
 vows.describe('Badge Validator').addBatch({
-  'When validating anything': {
-    topic: function() { return validate({}); },
-    'we get a structure': {
-      'with a `status` member': function(topic) {
-        assert.include(topic, 'status')
-      },
-      'with a `errors` member': function(topic) {
-        assert.include(topic, 'errors')
-      }
-    }
-  },
   'When validating something invalid': {
-    topic: function() { return validate({}); },
-    'we get a bad status and errors': function(topic) {
-      assert.equal(topic.status, 'failure');
-      assert.ok(Object.keys(topic.errors).length > 0);
+    topic: function() { new Badge({}).validate(this.callback); },
+    'we get errors': function(err, suc) {
+      assert.ok(err);
+      assert.instanceOf(err, Error);
     }
   },
   'Invalid badge assertion': {
-    'with bad recipient': generateErrorTests('recipient', 'email', BAD_EMAILS),
-    'with bad evidence': generateErrorTests('evidence', 'url', BAD_URLS),
+    'with bad recipient': generateErrorTests('recipient', 'regexp', BAD_EMAILS),
+    'with bad evidence': generateErrorTests('evidence', 'regexp', BAD_URLS),
     'with bad expires': generateErrorTests('expires', 'isodate', BAD_DATES),
     'with bad issued_on': generateErrorTests('expires', 'isodate', BAD_DATES),
-    'with bad badge.version': generateErrorTests('badge.version', 'regex', BAD_VERSIONS),
-    'with bad badge.name': generateErrorTests('badge.name', 'length', [genstring(500)]),
-    'with bad badge.description': generateErrorTests('badge.description', 'length', [genstring(500)]),
-    'with bad badge.image': generateErrorTests('badge.image', 'url', BAD_URLS),
-    'with bad badge.criteria': generateErrorTests('badge.criteria', 'url', BAD_URLS),
-    'with bad badge.issuer.name': generateErrorTests('badge.issuer.name', 'length', [genstring(500)]),
-    'with bad badge.issuer.org': generateErrorTests('badge.issuer.org', 'length', [genstring(500)]),
-    'with bad badge.issuer.contact': generateErrorTests('badge.issuer.contact', 'email', BAD_EMAILS),
-    'with bad badge.issuer.url': generateErrorTests('evidence', 'url', BAD_URLS)
+    'with bad badge.version': generateErrorTests('badge.version', 'regexp', BAD_VERSIONS),
+    'with bad badge.name': generateErrorTests('badge.name', 'maxlen', [genstring(500)]),
+    'with bad badge.description': generateErrorTests('badge.description', 'maxlen', [genstring(500)]),
+    'with bad badge.image': generateErrorTests('badge.image', 'regexp', BAD_URLS),
+    'with bad badge.criteria': generateErrorTests('badge.criteria', 'regexp', BAD_URLS),
+    'with bad badge.issuer.name': generateErrorTests('badge.issuer.name', 'maxlen', [genstring(500)]),
+    'with bad badge.issuer.org': generateErrorTests('badge.issuer.org', 'maxlen', [genstring(500)]),
+    'with bad badge.issuer.contact': generateErrorTests('badge.issuer.contact', 'regexp', BAD_EMAILS),
+    'with bad badge.issuer.url': generateErrorTests('evidence', 'regexp', BAD_URLS)
   },
   'Valid badge assertion' : {
     'without issued_on': {
-      topic: fixture({issued_on: null}),
-      'should not have errors': function(topic){
-        var result = validate(topic);
-        assert.equal(result.status, 'success');
-        assert.equal(Object.keys(result.errors).length, 0);
+    topic: function() { new Badge(fixture({issued_on: null})).validate(this.callback); },
+      'should not have errors': function(err, succ){
+        assert.equal(err, null)
       }
     }
   }
