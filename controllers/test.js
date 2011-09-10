12a01da4a7ab50a82d7f9dd3136afb3b5571539f
Brian J Brennan <brianloveswords@gmail.com>
2011-09-09 21:43:58 -0400

    Allow badge.json to be modified with test data.

--- controllers/test.js
+++ controllers/test.js
@@ -1,26 +1,36 @@
 var remote = require('../remote')
   , configuration = require('../lib/configuration')
-
-console.log('loaded');
+exports.issuer = function(req, res) {
+  res.render('issuer', {
+    login: false,
+    title: 'Test Issuer'
+  });
+}
 
 exports.test_badge = function(req, res) {
-  var protocol = configuration.get('protocol') || 'http';
-  var port = configuration.get('external_port') || '';
+  var protocol = configuration.get('protocol') || 'http'
+    , port = configuration.get('external_port') || ''
+  
+  var title = req.query.title || 'Open Source Contributor'
+    , image = req.query.image || '/images/test-badge.png'
+    , desc = req.query.desc || 'For rocking in the free world'
+    , recp = req.query.recp || 'me@example.com'
+  
   setTimeout(function(){
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({
-      recipient: 'me@example.com',
+      recipient: recp,
       evidence: '/test/evidence',
       expires: '2040-08-13',
       issued_on: '2011-08-23',
       badge: {
         version: 'v0.5.0',
-        name: 'Open Source Contributor',
-        description: 'For rocking in the free world',
-        image: '/images/test-badge.png',
+        name: 'TEST: ' + title,
+        description: desc,
+        image: image,
         criteria: '/test/criteria',
         issuer: {
-          name: 'Open Badges',
+          name: 'Open Badges Test',
           origin: protocol + '://' + configuration.get('hostname') + (port? ':' + port : '')
         }
       }
