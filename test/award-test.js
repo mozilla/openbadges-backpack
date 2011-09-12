d756c075f4aacd4d561316e8de93de36e6372efa
Brian J Brennan <brianloveswords@gmail.com>
2011-09-12 16:30:35 -0400

    Derive public badge path from configuration [closes #6]

--- test/award-test.js
+++ test/award-test.js
@@ -5,26 +5,32 @@ var vows = require('./setup')
   , path = require('path')
   , assertion = require('./utils').fixture()
   , Badge = require('../models/badge')
+  , configuration = require('../lib/configuration')
+  , badgeDir = configuration.get('badge_path')
 
 var PNGFILE = path.join(__dirname, 'no-badge-data.png')
   , PNGDATA = fs.readFileSync(PNGFILE)
 
 Badge.collection.drop();
 vows.describe('Awarding Badges').addBatch({
-  'A valid badge' : {
+  'An awarded badge' : {
     topic: function() { award(assertion, 'http://example.com/this-badge', PNGDATA, this.callback) },
-    'can be awarded': function(err, badge){
+    'gets awarded without error': function(err, badge){
       assert.ifError(err);
     },
-    'and updated' : {
+    'can be retrieved' : {
       topic: function(err, badge) {
         var self = this;
         award(assertion, 'http://example.com/this-badge', PNGDATA, function(err, badge){
           Badge.find({'meta.pingback': 'http://example.com/this-badge'}, self.callback)
         });
       },
-      'without duplicating': function(err, docs) {
+      'and updated without duplicating': function(err, docs) {
         assert.length(docs, 1);
+      },
+      'and has expected imagePath': function(err, docs) {
+        var path = badgeDir.replace(/^.*?static/, '');
+        assert.ok(docs[0].meta.imagePath.match(path));
       }
     },
   },
