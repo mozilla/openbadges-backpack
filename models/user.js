81ab5de234bb5fd2c19eb1567ee950c7de04e62b
Brian J Brennan <brianloveswords@gmail.com>
2011-09-13 21:47:24 -0400

    New user model.

---
+++ models/user.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose')
+  , conf = require('../lib/configuration').get('database')
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId
+  
+mongoose.connect(conf.host, conf.name, conf.port);
+
+var Group = new Schema(
+  { name: { type: String }
+  , badges: [ObjectId]
+  }
+)
+var User = new Schema(
+  { email: String
+  , groups: [Group]
+  }
+)
+
+var UserModel = module.exports = mongoose.model('User', User);
+
