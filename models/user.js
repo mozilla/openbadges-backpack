fc81ae443ba510058201130aea65334f3e6961e3
Brian J Brennan <brianloveswords@gmail.com>
2011-12-13 12:18:39 -0800

    Beginning user model rewrite: scaffold methods, starting testing.

--- models/user.js
+++ models/user.js
@@ -1,67 +1,29 @@
-// #TODO: un-insane this. This is relational data and should be stored in a
-// relational database (sqlite, postgre or mysql), not in mongo (or any other
-// nosql database).
-//
-// This needs to be fixed before doing display API or facebook integration.
-var mongoose = require('mongoose')
-  , conf = require('../lib/configuration').get('database')
-  , Badge = require('./badge')
-var Schema = mongoose.Schema
-  , ObjectId = Schema.ObjectId
+var mysql = require('../lib/mysql')
+  , client = mysql.client;
 
-mongoose.connect(conf.host, conf.name, conf.port);
+var User = function(data){
+  this.data = data;
+}
 
-var Group = new Schema(
-  { name: { type: String }
-  , badges: [ObjectId]
-  }
-)
-var User = new Schema(
-  { email: { type: String }
-  , groups: [Group]
-  }
-)
-var UserModel = module.exports = mongoose.model('User', User);
-UserModel.prototype.populateGroups = function(callback) {
-  if (!this.groups.length) return callback();
-  var times = {}
-  times.amount = this.groups.length;
-  times.hit = function() {
-    times.amount -= 1;
-    if (!times.amount) callback()
-  }
-  
-  this.groups.forEach(function(g) {
-    Badge.find({'_id': {'$in': g.badges}}, function(err, docs) {
-      if (err) return callback(err)
-      g.realBadges = docs;
-      return times.hit();
-    })
-  })
+User.prototype.save = function() { }
+User.prototype.destroy = function() { }
+User.prototype.collections = function() { }
+User.prototype.createCollection = function(data) { }
+User.prototype.saved = false;
+User.findOrCreate = function() {  }
+User.find = function(criteria) {  }
+
+var Collection = function(data){
+  this.data = data;
 }
-UserModel.prototype.groupExists = function(name) {
-  return this.groups.some(function(g){ return g.name.toLowerCase() === name.toLowerCase() })
-}
-UserModel.prototype.updateBadgeGroups = function(badge, keep, newGroup, callback) {
-  var updated = [];
-  this.groups.forEach(function(g){
-    if (!(g.id in keep)) {
-      g.badges = g.badges.filter(function(b){ return b.toString() !== badge.id; });
-      if (g.badges.length) updated.push(g);
-    } else {
-      if (g.badges.indexOf(badge.id) === -1) g.badges.push(badge.id);
-      updated.push(g);
-    }
-  })
-  if (newGroup) {
-    var exists = this.groups.map(function(g){ return g.name.toLowerCase(); }).indexOf(newGroup.toLowerCase()) !== -1
-    if (!exists) {
-      this.groups.push({
-        name: newGroup,
-        badges: [ badge.id ]
-      })
-      updated.push(this.groups.pop());
-    }
-  }
-  UserModel.update({_id: this.id}, {'$set': {'groups': updated}}, callback)
-}
+
+
+// mysql.useTestDatabase();
+
+/* 
+ user = User.find({email: 'whaaaat'})
+ user.collections() -> [
+   Collection({ name: 'what', badges: [Badge(...), Badge(...)], user: User(...)})
+ ]
+
+*/
---
+++ test/user-models-test.js
@@ -0,0 +1,3 @@
+var mysql = require('../lib/mysql')
+mysql.prepareTesting();
+
