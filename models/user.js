82e26b6aa899806e5af4977c1701b6d425e713d0
Brian J Brennan <brianloveswords@gmail.com>
2011-12-13 15:59:21 -0800

    Implemented new user saving & updating.

--- models/user.js
+++ models/user.js
@@ -1,11 +1,24 @@
 var mysql = require('../lib/mysql')
-  , client = mysql.client;
 
 var User = function(data){
+  this.fields = ['id', 'email', 'passwd', 'last_login', 'active'];
   this.data = data;
+  this.client = mysql.client;
 }
 
-User.prototype.save = function() { }
+User.prototype.save = function(callback) {
+  var data = this.data
+    , client = this.client
+    , self = this;
+  
+  client._upsert(User._table, data, function (err, result) {
+    if (err) return callback(err, null);
+    if (!data.id && result.insertId) data.id = result.insertId;
+    return callback(null, self);
+  })
+  
+}
+
 User.prototype.destroy = function() { }
 User.prototype.collections = function() { }
 User.prototype.createCollection = function(data) { }
@@ -13,10 +26,13 @@ User.prototype.saved = false;
 User.findOrCreate = function() {  }
 User.find = function(criteria) {  }
 
+User._table = 'user';
+
 var Collection = function(data){
   this.data = data;
 }
 
+module.exports = User;
 
 // mysql.useTestDatabase();
 
--- test/user-models-test.js
+++ test/user-models-test.js
@@ -1,3 +1,36 @@
-var mysql = require('../lib/mysql')
+var vows = require('./setup')
+  , assert = require('assert')
+  , mysql = require('../lib/mysql')
+  , User = require('../models/user')
+
+// all queries get queued and executed in series.
 mysql.prepareTesting();
 
+vows.describe('user model').addBatch({
+  'When saving a new user': {
+    topic: function () {
+      var bimmy = new User({email: 'bimmy@example.com'})
+      bimmy.save(this.callback);
+    },
+    'no errors are thrown': function (err, user) { assert.ifError(err) },
+    'user object': {
+      topic: function (user) { return user },
+      'is returned': function (user) { assert.ok(user) },
+      'contains id': function (user) { assert.ok(user.data.id) },
+      'contains email': function (user) { assert.equal(user.data.email, 'bimmy@example.com') },
+      'can be updated': {
+        topic: function (user) {
+          user.data.active = 0;
+          user.save(this.callback);
+        },
+        'without error': function (err, user) { assert.ifError(err) },
+        'and return the right data': function (err, user) { assert.equal(user.data.active, 0) }
+      },
+      'can be found': {
+        topic: function(user) {
+          return user;
+        }
+      }
+    }
+  }
+}).export(module)
