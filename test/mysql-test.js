c6138591f854307d497ef51a3753c1b213abeece
Brian J Brennan <brianloveswords@gmail.com>
2012-01-23 13:34:14 -0500

    Can create databases for testing.

--- test/mysql-test.js
+++ test/mysql-test.js
@@ -1,14 +1,20 @@
 var vows = require('vows')
   , assert = require('assert')
   , mysql = require('../lib/mysql')
-  , client = mysql.client;
+  , client = mysql.client
+  , conf = require('../lib/configuration').get('database')
+  , testDb = conf.database + "_test";
 
 vows.describe('Testing database').addBatch({
-  "taaaaaaables": {
-    topic: function () { client.query('show databases', this.callback) },
-    'can be whaaaat': function (err, data) {
-      console.dir(data);
-      assert.ok(true);
+  "When running test prep": {
+    topic: function () {
+      mysql.prepareTesting();
+      client.query('show databases', this.callback);
+    },
+    'can be created': function (err, results) {
+      var databases = results.map(function (a) {return a.Database})
+        , testDbExists = (databases.indexOf(testDb) != -1);
+      assert.ok(testDbExists);
     } 
   }
 }).export(module);
