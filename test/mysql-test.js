6661d220341e60aade5eccd31a2995cb7d8f13d4
Brian J Brennan <brianloveswords@gmail.com>
2012-01-23 14:22:48 -0500

    Testing table creation.

--- test/mysql-test.js
+++ test/mysql-test.js
@@ -5,17 +5,32 @@ var vows = require('vows')
   , conf = require('../lib/configuration').get('database')
   , testDb = conf.database + "_test";
 
+Object.values = function (obj) { return Object.keys(obj).map(function (k) { return obj[k]; }) };
+var extractFirstValues = function (arr) { return arr.map(function (a) { return Object.values(a).pop(); }) }
+var toObj = function (arr) { var r = {}; arr.forEach(function (a) { r[a] = 1; }); return r; }
+
+mysql.prepareTesting();
 vows.describe('Testing database').addBatch({
-  "When running test prep": {
+  "After running test prep": {
     topic: function () {
-      mysql.prepareTesting();
       client.query('show databases', this.callback);
     },
-    'can be created': function (err, results) {
-      var databases = results.map(function (a) {return a.Database})
-        , testDbExists = (databases.indexOf(testDb) != -1);
-      assert.ok(testDbExists);
-    } 
+    'proper database is created': function (err, results) {
+      var databases = extractFirstValues(results);
+      assert.includes(databases, testDb);
+    },
+    'expected tables': {
+      topic: function () {
+        client.query('show tables', this.callback);
+      },
+      'are created': function (err, results) {
+        var tables = extractFirstValues(results);
+        assert.equal(mysql.schemas.length, tables.length);
+        // #TODO: should not hardcode these.
+        ['badge', 'user', 'collection', 'collection_badge'].forEach(function (t) {
+          assert.includes(tables, t);
+        })
+      }
+    }
   }
 }).export(module);
-
