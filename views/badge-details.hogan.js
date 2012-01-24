e42248cadf77bdda89a265cf9cf7d85e41659043
Chris McAvoy <chris.mcavoy@gmail.com>
2012-01-24 17:01:07 -0600

    one additional file

--- views/badge-details.hogan.js
+++ views/badge-details.hogan.js
@@ -67,3 +67,60 @@
     </div>
   </div>
 </div>
+
+<script type="text/javascript">
+(function() {
+
+  coffeescript(function() {
+    var autocheck, checkboxes, newGroup, shortDisable, watchChanges;
+    newGroup = $('#new-group');
+    checkboxes = $('.input-append input[type=checkbox]');
+    watchChanges = function(event) {
+      var elem, input, label;
+      elem = $(this);
+      label = elem.parent();
+      input = label.siblings('input').first();
+      if (elem.attr('checked')) {
+        label.addClass('active');
+        if (!input.val()) return input.trigger('focus');
+      } else {
+        return label.removeClass('active');
+      }
+    };
+    autocheck = function(event) {
+      var checkbox, checked, elem;
+      elem = $(this);
+      checkbox = elem.siblings('label').first().find('input');
+      checked = elem.val() ? true : false;
+      return checkbox.attr('checked', checked).trigger('change');
+    };
+    shortDisable = function() {
+      var checkbox, elem;
+      elem = $(this);
+      checkbox = elem.siblings('label').first().find('input');
+      checkbox.attr('disabled', true);
+      return setTimeout(function() {
+        return checkbox.attr('disabled', false);
+      }, 20);
+    };
+    checkboxes.bind('change', watchChanges).trigger('change');
+    return newGroup.bind('keydown', autocheck).bind('blur', autocheck).bind('blur', shortDisable);
+  });
+</script>
+<script>
+(function() {
+
+  coffeescript(function() {
+    var image;
+    image = $('#badge-image');
+    return image.bind('load', function(event) {
+      if (this.clientWidth > 256) {
+        return $(this).css({
+          width: '256px'
+        });
+      }
+    });
+  });
+
+}).call(this);
+</script>
