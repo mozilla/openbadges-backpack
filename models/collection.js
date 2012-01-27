d5db316aced8be7e53ce23264840410169faa245
Brian J Brennan <brianloveswords@gmail.com>
2012-01-26 19:16:01 -0500

    First pass at collection model.

--- models/collection.js
+++ models/collection.js
@@ -1,10 +1,22 @@
 var mysql = require('../lib/mysql'),
+    Badge = require('./badge'),
     Base = require('./mysql-base.js');
 
-var Collection = function (data) { this.data = data; };
+var Collection = function (data) {
+  this.data = data;
+};
 Base.apply(Collection, 'collection');
-
-var CollectionBadge = function (data) { this.data = data; };
-Base.apply(CollectionBadge, 'collection_badge');
+Collection.prototype.getBadgeObjects = function (callback) {
+  var badgeIds = this.data.badges.slice(0),
+      values = badgeIds,
+      query = 'SELECT * FROM `badge` WHERE `id` IN ('
+    + badgeIds.map(function(){return '?';}).join(',')
+    + ') AND `user_id` = ?';
+  values.push(this.data.user_id);
+  return this.client.query(query, values, function (err, results) {
+    if (err) { return callback(err); }
+    callback(null, results.map(Badge.fromDbResult));
+  });
+}
 
 module.exports = Collection;
--- test/collection-model-test.js
+++ test/collection-model-test.js
@@ -1,11 +1,13 @@
 var vows = require('vows'),
     assert = require('assert'),
     mysql = require('../lib/mysql'),
+    genstring = require('./utils').genstring,
     Collection = require('../models/collection');
 
 var createDbFixtures = function (callback) {
   var addUser = "INSERT INTO `user` (email) VALUES ('brian@example.com')";
-  var addBadge1 = "INSERT INTO `badge`"
+  var addUser2 = "INSERT INTO `user` (email) VALUES ('brian2@example.com')";
+  var addBadge = "INSERT INTO `badge`"
     + "(user_id, type, endpoint, image_path, body, body_hash)"
     + "VALUES"
     + "(1, 'hosted', 'http://example.com', '/dev/null', '{\"wut\":\"lol\"}', 'sha256$lol')";
@@ -14,10 +16,18 @@ var createDbFixtures = function (callback) {
     + "VALUES"
     + "(1, 'hosted', 'http://example.com/yaaaa.json', '/dev/null', '{\"dummy\":\"data\"}', 'sha256$rad')";
   mysql.client.query(addUser);
-  mysql.client.query(addBadge1);
+  mysql.client.query(addUser2);
+  mysql.client.query(addBadge);
   mysql.client.query(addBadge2, callback);
 };
 
+var createCollection = function () {
+  return new Collection({
+    user_id: 1,
+    name: 'test collection',
+    url: genstring(64)
+  })
+};
 vows.describe('Collllleccctions').addBatch({
   'Collection testing:': {
     topic: function () {
@@ -25,13 +35,7 @@ vows.describe('Collllleccctions').addBatch({
       createDbFixtures(this.callback);
     },
     'A valid new collection': {
-      topic: function () {
-        return new Collection({
-          user_id: 1,
-          name: 'test collection',
-          url: 'whaaaaat'
-        })
-      },
+      topic: createCollection(),
       'can be saved': {
         topic: function (collection) {
           collection.save(this.callback)
@@ -39,7 +43,21 @@ vows.describe('Collllleccctions').addBatch({
         'without errors': function (err, collection) {
           assert.ifError(err);
           assert.isObject(collection);
-          console.dir(collection);
+        }
+      }
+    },
+    'A collection' : {
+      topic: createCollection(),
+      'can have badges added to it before saving': {
+        topic: function (collection) {
+          collection.data.badges = [1,2];
+          collection.save(function (err, collection) {
+            collection.getBadgeObjects(this.callback);
+          }.bind(this))
+        },
+        'and have them be there when retrieving': function (err, badges) {
+          console.dir(badges);
+          assert.equal(badges.length, 2);
         }
       }
     }
