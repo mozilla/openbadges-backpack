7b96dd366b736bc7691d003bde63897bf9d5da59
Brian J Brennan <brianloveswords@gmail.com>
2012-01-30 00:28:37 -0500

    Put setup into the test harness.

--- test/award-test.js
+++ test/award-test.js
@@ -12,25 +12,32 @@ var vows = require('vows')
 var PNGFILE = path.join(__dirname, 'no-badge-data.png')
   , PNGDATA = fs.readFileSync(PNGFILE)
 
-mysql.prepareTesting();
 vows.describe('Awarding Badges').addBatch({
-  'An awarded badge' : {
-    topic: function() { award(assertion, 'http://example.com/this-badge', PNGDATA, this.callback) },
-    'gets awarded without error': function(err, badge){
-      assert.ifError(err);
+  'Awarding:': {
+    topic: function () {
+      mysql.prepareTesting();
+      return true;
     },
-    'can be retrieved' : {
-      topic: function(err, badge) {
-        award(assertion, 'http://example.com/this-badge', PNGDATA, function(err, badge){
-          Badge.find({'endpoint': 'http://example.com/this-badge'}, this.callback)
-        }.bind(this));
+    'An awarded badge' : {
+      topic: function() {
+        award(assertion, 'http://example.com/this-badge', PNGDATA, this.callback)
       },
-      'and updated without duplicating': function(err, badges) {
-        assert.equal(badges.length, 1);
+      'gets awarded without error': function(err, badge){
+        assert.ifError(err);
       },
-      'and has expected imagePath': function(err, badges) {
-        var path = badgeDir.replace(/^.*?static/, '');
-        assert.ok(badges[0].data.image_path.match(path));
+      'can be retrieved' : {
+        topic: function(err, badge) {
+          award(assertion, 'http://example.com/this-badge', PNGDATA, function(err, badge){
+            Badge.find({'endpoint': 'http://example.com/this-badge'}, this.callback)
+          }.bind(this));
+        },
+        'and updated without duplicating': function(err, badges) {
+          assert.equal(badges.length, 1);
+        },
+        'and has expected imagePath': function(err, badges) {
+          var path = badgeDir.replace(/^.*?static/, '');
+          assert.ok(badges[0].data.image_path.match(path));
+        }
       }
     }
   }
