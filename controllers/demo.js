d6b1f9827b30f30fa480e319c99f8f82c57ff884
Brian J Brennan <brianloveswords@gmail.com>
2012-02-01 19:56:01 -0500

    Find and replace: "test" to  "demo"

--- controllers/demo.js
+++ controllers/demo.js
@@ -1,4 +1,4 @@
-// Controller for providing metadata for & awarding test badges.
+// Controller for providing metadata for & awarding demo badges.
 var qs = require('querystring')
   , configuration = require('../lib/configuration')
   , request = require('request')
@@ -7,16 +7,16 @@ var protocol = configuration.get('protocol') || 'http'
   , port = configuration.get('external_port') || ''
   , ORIGIN = protocol + '://' + configuration.get('hostname') + (port? ':' + port : '');
 
-// Render the view for the test badge issuer.
+// Render the view for the demo badge issuer.
 exports.issuer = function(req, res) {
   res.render('issuer', {
     login: false,
-    title: 'Test Issuer',
+    title: 'Demo Issuer',
     csrfToken: req.session._csrf
   });
 }
 
-// Bake & award a test badge. Uses `test_badge` below to generate a proper assertion.
+// Bake & award a demo badge. Uses `demo_badge` below to generate a proper assertion.
 exports.award = function(req, res) {
   var assertionURL = encodeURIComponent([ORIGIN + '/demo/badge.json', qs.stringify(req.body)].join('?'))
     , bakeURL = ORIGIN + '/baker?award=true&assertion=' + assertionURL;
@@ -27,36 +27,33 @@ exports.award = function(req, res) {
 
 }
 
-// Create a test badge. Optionally override default values by providing GET
+// Create a demo badge. Optionally override default values by providing GET
 // params. This less an avenue for fraud than it might look: the name will
-// always contain "TEST" and the issuer will always be the backpack host.
-exports.test_badge = function(req, res) {
-  var title = req.query.title || 'Test Badge'
-    , image = req.query.image || '/images/test-badge.png'
+// always contain "DEMO" and the issuer will always be the backpack host.
+exports.demo_badge = function(req, res) {
+  var title = req.query.title || 'Demo Badge'
+    , image = req.query.image || '/images/demo-badge.png'
     , desc = req.query.desc || 'For rocking in the free world'
     , recp = req.query.recp || 'me@example.com';
 
-  // Use a timer to fake latency.
-  setTimeout(function(){
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({
-      recipient: recp,
-      evidence: '/demo/evidence',
-      expires: '2040-08-13',
-      issued_on: '2011-08-23',
-      badge: {
-        version: 'v0.5.0',
-        name: 'TEST: ' + title,
-        description: desc,
-        image: image,
-        criteria: '/demo/criteria',
-        issuer: {
-          name: 'Open Badges Test',
-          origin: ORIGIN
-        }
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    recipient: recp,
+    evidence: '/demo/evidence',
+    expires: '2040-08-13',
+    issued_on: '2011-08-23',
+    badge: {
+      version: 'v0.5.0',
+      name: 'DEMO: ' + title,
+      description: desc,
+      image: image,
+      criteria: '/demo/criteria',
+      issuer: {
+        name: 'Open Badges Demo',
+        origin: ORIGIN
       }
-    }));
-  }, 200);
+    }
+  }));
 }
 
 // Send back a bad assertion after a timeout to simulate latency.
