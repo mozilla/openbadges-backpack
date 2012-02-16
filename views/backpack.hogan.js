adc612d594a75e04748641b4c3b62241f8c6c052
Brian J Brennan <brianloveswords@gmail.com>
2012-02-16 15:55:49 -0500

    Style and sample HTML for badge details lightbox.

--- views/backpack.hogan.js
+++ views/backpack.hogan.js
@@ -62,20 +62,81 @@
   {{/badges.length}}
 </div>
 
+{{=|| ||=}} <!-- need to change delimeter so hogan doesn't parse these --->
 
-<div class='blanker'>
+<div class='lightbox'>
   <div class='contents badge-details'>
     <header>
-      <h2>Details</h2>
+      <h2>Awesome Badge of Awesomeitudiness</h2>
     </header>
-  
+    <div class='body'>
+      <table class='information'>
+        
+        <tr>
+          <td rowspan="100" class='image'>
+            <img src="/_badges/6e20f188a75052fc5b6a573121e428fa.png">
+            <button class='btn danger'>Disown this Badge</button>
+          </td>
+          
+          <td class='section-head' colspan='2'>Issuer Details</td>
+        </tr>
+        <tr>
+          <td class='label issuer-name'>Name</td>
+          <td>P2PU</td>
+        </tr>
+        <tr>
+          <td class='label issuer-name'>URL</td>
+          <td><a href='http://p2pu.org'>http://p2pu.org</a></td>
+        </tr>
+        <tr>
+          <td class='label issuer-name'>Organization</td>
+          <td>School of Webcraft</td>
+        </tr>
+        
+        <tr>
+          <td class='section-head' colspan='2'>Badge Details</td>
+        </tr>
+        <tr>
+          <td class='label'>Name</td>
+          <td>Awesome Badge of Awesomeitudiness</td>
+        </tr>
+        <tr>
+          <td class='label'>Description</td>
+          <td>For keeping it real and rocking in the free world.</td>
+        </tr>
+        <tr>
+          <td class='label'>Criteria</td>
+          <td><a href='#'>http://example.com/awesome/</a></td>
+        </tr>
+        
+        <tr>
+          <td class='section-head' colspan='2'>Issuance Details</td>
+        </tr>
+        <tr>
+          <td class='label recipient'>Recipient</td>
+          <td>bimmy@example.com</td>
+        </tr>
+        <tr>
+          <td class='label evidence'>Evidence</td>
+          <td><a href='#'>http://example.com/what/</a></td>
+        </tr>
+        <tr>
+          <td class='label'>Issued On</td>
+          <td>January 1st, 2001</td>
+        </tr>
+        <tr>
+          <td class='label'>Expiration Date</td>
+          <td>January 1st, 2013</td>
+        </tr>
+      
+      </table>
+    </div>
   </div>
 </div>
 
 
 
 
-{{=|| ||=}} <!-- need to change delimeter so hogan doesn't parse these --->
 <script type='text/html' id='groupTpl'>
   <div class='group {{^attributes.id}}isNew{{/attributes.id}}'>
     <input class='groupName' type='text' value='{{name}}'>
