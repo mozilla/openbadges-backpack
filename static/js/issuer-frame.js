426a4d70c0a0a54545d068d604aeadf955f121fa
Atul Varma <varmaa@gmail.com>
2012-02-24 07:20:56 -0500

    styled issuer-frame, added close button, change-user button, etc.

--- static/js/issuer-frame.js
+++ static/js/issuer-frame.js
@@ -1,44 +1,75 @@
-var CSRF = $("meta[http-equiv='X-CSRF-Token']").attr("content");
-var currentUser = $("meta[http-equiv='X-Current-User']").attr("content");
+var Session = (function() {
+  var loginStarted = false;
+  var Session = {
+    CSRF: $("meta[http-equiv='X-CSRF-Token']").attr("content"),
+    currentUser: $("meta[http-equiv='X-Current-User']").attr("content"),
+    login: function() {
+      if (!Session.currentUser && !loginStarted) {
+        navigator.id.getVerifiedEmail(function(assertion) {
+          jQuery.ajax({
+            url: '/backpack/authenticate',
+            type: 'POST',
+            dataType: 'json',
+            data: {assertion: assertion},
+            success: function(data) {
+              Session.currentUser = data.email;
+              Session.trigger("login-complete");
+            },
+            error: function() {
+              Session.trigger("login-error");
+            },
+            complete: function() {
+              loginStarted = false;
+            }
+          });
+        });
+        loginStarted = true;
+        Session.trigger('login-started');
+      }
+    }
+  };
 
-function showLogin() {
-  $(".logged-out, .logged-in").hide();
-  if (!currentUser) {
-    $(".logged-out").show();
-  } else {
-    $(".logged-in").show();
-  }
+  _.extend(Session, Backbone.Events);
+
+  jQuery.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+      if (!settings.crossDomain && settings.type != "GET")
+        xhr.setRequestHeader('X-CSRF-Token', Session.CSRF)
+    }
+  });
+
+  return Session;
+})();
+
+function showBadges() {
+  $("#welcome").fadeOut(function() {
+    
+  });
 }
 
-showLogin();
-
-$(".js-browserid-link").click(function() {
-  navigator.id.getVerifiedEmail(function(assertion) {
-    jQuery.ajax({
-      url: '/backpack/authenticate',
-      type: 'POST',
-      dataType: 'json',
-      data: {
-        _csrf: CSRF,
-        assertion: assertion
-      },
-      success: function(data) {
-        currentUser = data.email;
-        showLogin();
-      },
-      error: function() {
-        console.log("uhoh");
-      }
+$(window).ready(function() {
+  if (!Session.currentUser) {
+    $(".logged-out").show();
+    Session.on("login-complete", showBadges);
+    $(".logged-out .js-browserid-link").click(function() {
+      Session.login();
+      return false;
     });
-    // TODO: Display a throbber or something.
-  });
-  return false;
-});
+  } else {
+    $(".logged-in").show();
+    $(".logged-in .next").click(showBadges);
+    $(".logged-in .email").text(Session.currentUser);
+    $(".logged-in .logout").click(function() {
+      alert("TODO: Log out the user.");
+      return false;
+    });
+  }
 
-var channel = Channel.build({
-  window: window.parent,
-  origin: "*",
-  scope: "OpenBadges.issue"
+  $(".host").text(window.location.host);
+  $(".topbar .close").click(function() {
+    alert("TODO: Close window and send response to parent.");
+    return false;
+  });
 });
 
 // This is the core issuing implementation; the response is proxied
@@ -53,9 +84,22 @@ function issue(assertions, cb) {
   setTimeout(function() { cb(errors, []); }, 10);
 }
 
-channel.bind("issue", function(trans, s) {
-  issue(s, function(errors, successes) {
-    trans.complete([errors, successes]);
+var channel = (function() {
+  if (window.parent === window)
+    return null;
+  
+  var channel = Channel.build({
+    window: window.parent,
+    origin: "*",
+    scope: "OpenBadges.issue"
   });
-  trans.delayReturn(true);
-});
+
+  channel.bind("issue", function(trans, s) {
+    issue(s, function(errors, successes) {
+      trans.complete([errors, successes]);
+    });
+    trans.delayReturn(true);
+  });
+  
+  return channel;
+})();
