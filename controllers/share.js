9d0fa300fc5cac4dbea2b7fc1ca41f945c1eaed7
Brian J Brennan <brianloveswords@gmail.com>
2012-02-27 04:16:29 -0800

    Add portfolio model.

--- controllers/share.js
+++ controllers/share.js
@@ -1,5 +1,6 @@
 var User = require('../models/user')
   , Badge = require('../models/badge')
+  , Portfolio = require('../models/portfolio')
   , Group = require('../models/group')
   , reverse = require('../lib/router').reverse
   , configuration = require('../lib/configuration')
@@ -25,12 +26,10 @@ exports.param = {
 }
 
 
-
 var testStruct = {
   "title": "My Creative Commons Badges",
   "subtitle": "Some subtitle here",
   "preamble": "Nulla seitan excepteur, dreamcatcher culpa anim banh mi 3 wolf moon. Commodo sunt VHS flexitarian gastropub craft beer chambray, mlkshk stumptown biodiesel mollit organic nisi. Butcher sapiente direct trade velit, aute dolore vero. Truffaut kogi 3 wolf moon, forage carles ennui elit culpa typewriter portland DIY locavore. Terry richardson DIY hella delectus tofu. Non ex fap, sriracha lo-fi aute ethnic mumblecore cupidatat bushwick you probably haven't heard of them. Cred odio direct trade sed, aliquip four loko cardigan occaecat marfa beard anim letterpress.",
-  //  "postamble": "some more stuff",
   "stories": {
     "7" : "Mollit aliquip anim irony, mlkshk small batch esse laborum tofu eiusmod synth cupidatat art party typewriter. Adipisicing nulla banksy quis kogi. Pour-over quinoa carles sint DIY, occaecat gastropub synth cardigan occupy truffaut readymade cosby sweater etsy. Beard food truck velit pinterest organic post-ironic, truffaut +1 gentrify eu. Street art locavore DIY authentic. Semiotics terry richardson godard pinterest letterpress est, irure occaecat kogi. Small batch four loko williamsburg lomo odd future pop-up.",
     "8" : "Butcher raw denim ex occaecat, hella consectetur pork belly ut portland freegan. Occupy enim wolf, ea yr sunt mlkshk culpa fugiat echo park sapiente salvia fap single-origin coffee. Excepteur tempor fixie, ennui thundercats do kale chips food truck american apparel voluptate lo-fi bushwick. Accusamus ennui tumblr, high life godard small batch letterpress dolor keytar est culpa labore. Typewriter fingerstache helvetica, anim eu american apparel four loko. Mlkshk velit brunch sint, fap aliqua 8-bit commodo odio cosby sweater enim quis. Cardigan squid cosby sweater 8-bit salvia, helvetica mustache odd future cillum sriracha irure reprehenderit hella.",
