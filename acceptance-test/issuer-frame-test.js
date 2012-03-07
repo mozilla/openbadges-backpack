8bdf30a83b4f85ac1d4498217dd4abedc653e92a
Atul Varma <varmaa@gmail.com>
2012-03-07 18:45:06 -0500

    more issuer-test acceptance tests

--- acceptance-test/issuer-frame-test.js
+++ acceptance-test/issuer-frame-test.js
@@ -18,15 +18,33 @@ browser.on('command', function(cmd, args){
 browser
   .chain
   .session()
-  .open('/issuer/frame')
-  .waitForPageToLoad(8000)
-  .click("css=#welcome button.next.btn.primary")
-  .waitForVisible('css=#badge-ask img[src="/_demo/cc.large.png"]')
-  .click("css=#badge-ask button.accept.btn.primary")
-  .waitForVisible('css=#badge-ask img[src="/_demo/by.large.png"]')
-  .click("css=#badge-ask button.accept.btn.primary")
-  .waitForVisible("css=#farewell h3.badges-many")
-  .click("css=#farewell button.next")
+  .open('/issuer/frame').waitForPageToLoad(8000)
+    // Accept 2 badges
+    .click("css=#welcome button.next.btn.primary")
+    .waitForVisible('css=#badge-ask img[src="/_demo/cc.large.png"]')
+    .click("css=#badge-ask button.accept")
+    .waitForVisible('css=#badge-ask img[src="/_demo/by.large.png"]')
+    .click("css=#badge-ask button.accept")
+    .waitForVisible("css=#farewell h3.badges-many")
+    .click("css=#farewell button.next")
+  .open('/issuer/frame').waitForPageToLoad(8000)
+    // Accept 1 badge, reject 1
+    .click("css=#welcome button.next.btn.primary")
+    .waitForVisible('css=#badge-ask img[src="/_demo/cc.large.png"]')
+    .click("css=#badge-ask button.reject")
+    .waitForVisible('css=#badge-ask img[src="/_demo/by.large.png"]')
+    .click("css=#badge-ask button.accept")
+    .waitForVisible("css=#farewell h3.badges-1")
+    .click("css=#farewell button.next")
+  .open('/issuer/frame').waitForPageToLoad(8000)
+    // Reject 2 badges
+    .click("css=#welcome button.next.btn.primary")
+    .waitForVisible('css=#badge-ask img[src="/_demo/cc.large.png"]')
+    .click("css=#badge-ask button.reject")
+    .waitForVisible('css=#badge-ask img[src="/_demo/by.large.png"]')
+    .click("css=#badge-ask button.reject")
+    .waitForVisible("css=#farewell h3.badges-0")
+    .click("css=#farewell button.next")
   .end(function(err) {
     if (err)
       throw err;
