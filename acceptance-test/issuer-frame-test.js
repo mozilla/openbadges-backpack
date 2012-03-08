e69db1f1b98037ae8f2a91a93e649d193b99133d
Atul Varma <varmaa@gmail.com>
2012-03-08 10:56:09 -0500

    refactored issuer-frame-test to be a bit more readable.

--- acceptance-test/issuer-frame-test.js
+++ acceptance-test/issuer-frame-test.js
@@ -1,39 +1,39 @@
+var soda = require('soda');
+
+soda.prototype.waitForBadgePrompt = function(img, action) {
+  return this.waitForVisible('css=#badge-ask img[src="/_demo/' + img + '"]')
+             .click("css=#badge-ask button." + action);
+};
+
 require('./acceptance-test-utils.js').createClient()
   .chain
   .session()
   .open('/issuer/frame').waitForPageToLoad(8000)
     // Log in as a different user and make backpack explode.
     .click("css=#welcome button.logout")
-    .waitForVisible('css=#badge-ask img[src="/_demo/nc.large.png"]')
-    .click("css=#badge-ask button.accept")
+    .waitForBadgePrompt("nc.large.png", "accept")
     .waitForVisible('css=#messages div.alert-message.danger')
     .waitForVisible("css=#farewell h3.badges-0")
     .click("css=#farewell button.next")
   .open('/issuer/frame').waitForPageToLoad(8000)
     // Accept 2 badges
     .click("css=#welcome button.next.btn.primary")
-    .waitForVisible('css=#badge-ask img[src="/_demo/cc.large.png"]')
-    .click("css=#badge-ask button.accept")
-    .waitForVisible('css=#badge-ask img[src="/_demo/by.large.png"]')
-    .click("css=#badge-ask button.accept")
+    .waitForBadgePrompt("cc.large.png", "accept")
+    .waitForBadgePrompt("by.large.png", "accept")
     .waitForVisible("css=#farewell h3.badges-many")
     .click("css=#farewell button.next")
   .open('/issuer/frame').waitForPageToLoad(8000)
     // Accept 1 badge, reject 1
     .click("css=#welcome button.next.btn.primary")
-    .waitForVisible('css=#badge-ask img[src="/_demo/cc.large.png"]')
-    .click("css=#badge-ask button.reject")
-    .waitForVisible('css=#badge-ask img[src="/_demo/by.large.png"]')
-    .click("css=#badge-ask button.accept")
+    .waitForBadgePrompt("cc.large.png", "reject")
+    .waitForBadgePrompt("by.large.png", "accept")
     .waitForVisible("css=#farewell h3.badges-1")
     .click("css=#farewell button.next")
   .open('/issuer/frame').waitForPageToLoad(8000)
     // Reject 2 badges
     .click("css=#welcome button.next.btn.primary")
-    .waitForVisible('css=#badge-ask img[src="/_demo/cc.large.png"]')
-    .click("css=#badge-ask button.reject")
-    .waitForVisible('css=#badge-ask img[src="/_demo/by.large.png"]')
-    .click("css=#badge-ask button.reject")
+    .waitForBadgePrompt("cc.large.png", "reject")
+    .waitForBadgePrompt("by.large.png", "reject")
     .waitForVisible("css=#farewell h3.badges-0")
     .click("css=#farewell button.next")
   .end(function(err) {
