c9bcf87ee4ccd0f47de9c7d61f32484ff4d0d28c
Brian J Brennan <brianloveswords@gmail.com>
2012-03-19 15:11:17 -0400

    Delete portfolio when deleting group.

--- controllers/group.js
+++ controllers/group.js
@@ -1,7 +1,8 @@
-var Group = require('../models/group')
-  , Badge = require('../models/badge')
-  , logger = require('../lib/logging').logger
-  , _ = require('underscore');
+var _ = require('underscore');
+var Group = require('../models/group.js');
+var Portfolio = require('../models/portfolio.js');
+var Badge = require('../models/badge.js');
+var logger = require('../lib/logging').logger;
 
 exports.param = {
   groupId: function(req, res, next, id) {
@@ -25,18 +26,22 @@ exports.create = function (req, res) {
   if (!req.user) return res.json({error:'no user'}, 403);
   if (!req.body) return res.json({error:'no badge body'}, 400);
   if (!req.body.badges) return res.json({error:'no badges'}, 400);
-  var user = req.user
-    , body = req.body
-    , badges = body.badges
-    , group
   function makeNewBadge(attributes) { return new Badge(attributes); }
-  group = new Group({
+  var user = req.user;
+  var body = req.body;
+  var badges = body.badges;
+  var group = new Group({
     user_id: user.get('id'),
     name: body.name,
     badges: badges.map(makeNewBadge)
   }); 
 
   group.save(function (err, group) {
+    if (err) {
+      logger.debug('there was some sort of error creating a group:');
+      logger.debug(err);
+      return res.send('there was an error', 500)
+    }
     res.contentType('json');
     res.send({id: group.get('id'), url: group.get('url')});
   })
@@ -44,10 +49,11 @@ exports.create = function (req, res) {
 
 exports.update = function (req, res) {
   if (!req.user) return res.send('nope', 403);
-  var group = req.group
-    , body = req.body
-    , saferName = body.name.replace('<', '&lt;').replace('>', '&gt;');
   function makeNewBadge(attributes) { return new Badge(attributes); }
+  var group = req.group;
+  var body = req.body;
+  var saferName = body.name.replace('<', '&lt;').replace('>', '&gt;');
+  
   group.set('name', saferName);
   group.set('badges', body.badges.map(makeNewBadge));
   group.save(function (err) {
@@ -57,16 +63,37 @@ exports.update = function (req, res) {
   })
 };
 
-exports.destroy = function (req, res) {
-  if (!req.user) return res.send('nope', 400);
-  var group = req.group
-  group.destroy(function (err) {
+exports.destroy = function (request, response) {
+  var user = request.user;
+  var group = request.group;
+  
+  if (!user)
+    return response.send('no logged in user', 403);
+  
+  if (!group)
+    return response.send('group not found', 404);
+  
+  if (group.get('user_id') !== user.get('id'))
+    return response.send('group not yours', 403);
+  
+  // find any profile associated with this group and delete it
+  Portfolio.findOne({group_id: group.get('id')}, function (err, folio) {
     if (err) {
-      logger.debug('some sort of error deleting!');
+      logger.debug('error finding portfolios:');
       logger.debug(err);
-      return res.send('nope', 500);
+      return response.send('nope', 500);
     }
-    res.contentType('json');
-    res.send(JSON.stringify({success: true}));
+    
+    if (folio) folio.destroy();
+    
+    group.destroy(function (err) {
+      if (err) {
+        logger.debug('error deleting group');
+        logger.debug(err);
+        return response.send('nope', 500);
+      }
+      response.contentType('json');
+      response.send(JSON.stringify({success: true}));
+    })
   })
 };
--- test/group-controller-test.js
+++ test/group-controller-test.js
@@ -7,15 +7,20 @@ var _ = require('underscore')
   , map = require('functools').map
   , conmock = require('./conmock.js')
 
-var user, badge, group;
+var user, otheruser, badge, group, othergroup, portfolio;
 function setupDatabase (callback) {
   var User = require('../models/user.js')
   var Badge = require('../models/badge.js')
   var Group = require('../models/group.js')
+  var Portfolio = require('../models/portfolio.js')
   var badgedata = require('../lib/utils').fixture({recipient: 'brian@example.com'})
   mysql.prepareTesting();
   function saver (m, cb) { m.save(cb) };
+  
   user = new User({ email: 'brian@example.com' })
+  
+  otheruser = new User({ email: 'lolwut@example.com' })
+  
   badge = new Badge({
     user_id: 1,
     type: 'hosted',
@@ -24,6 +29,7 @@ function setupDatabase (callback) {
     body_hash: 'body_hash',
     body: badgedata
   });
+  
   group = new Group({
     user_id: 1,
     name: 'name',
@@ -31,7 +37,23 @@ function setupDatabase (callback) {
     'public': 1,
     badges: [1]
   });
-  map.async(saver, [user, badge, group], callback);
+  
+  othergroup = new Group({
+    user_id: 1,
+    name: 'name',
+    url: 'url2',
+    'public': 1,
+    badges: []
+  });
+  
+  portfolio = new Portfolio({
+    group_id: 2,
+    url: 'url',
+    title: 'wut',
+    stories: '{"1": "oh hey"}'
+  });
+  
+  map.async(saver, [user, otheruser, badge, group, othergroup, portfolio], callback);
 }
 
 
@@ -76,10 +98,53 @@ vows.describe('group controller test').addBatch({
         conmock(groupcontroller.create, req, this.callback);
       },
       'creates a new group and returns id and url': function (err, mock) {
-        mock.body.id.should.equal(2);
+        mock.body.id.should.equal(3);
         should.exist(mock.body.url);
         mock.body.url.length.should.be.greaterThan(10);
       }
+    },
+    '#destroy: given no user': {
+      'topic' : function () {
+        conmock(groupcontroller.destroy, { group: group }, this.callback);
+      },
+      'fails with a 403': function (err, mock) {
+        mock.status.should.equal(403);
+      }
+    },
+    '#destroy: given no group': {
+      'topic' : function () {
+        conmock(groupcontroller.destroy, { user: user }, this.callback);
+      },
+      'fails with a 404': function (err, mock) {
+        mock.status.should.equal(404);
+      }
+    },
+    '#destroy: given the wrong user': {
+      'topic' : function () {
+        var req = { group: group, user: otheruser };
+        conmock(groupcontroller.destroy, req, this.callback);
+      },
+      'fails with a 403': function (err, mock) {
+        mock.status.should.equal(403);
+      }
+    },
+    '#destroy: given the right user': {
+      'topic' : function () {
+        var req = { group: group, user: user };
+        conmock(groupcontroller.destroy, req, this.callback);
+      },
+      'succeeds with a 200': function (err, mock) {
+        mock.status.should.equal(200);
+      }
+    },
+    '#destroy: given an empty group with a portfolio': {
+      'topic' : function () {
+        var req = { group: othergroup, user: user };
+        conmock(groupcontroller.destroy, req, this.callback);
+      },
+      'succeeds with a 200' : function (err, mock) {
+        mock.status.should.equal(200);
+      },
     },
   },
 }).export(module);
