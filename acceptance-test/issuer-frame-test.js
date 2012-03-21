12c45627f4835e75d5f58f26b9af188847deeaaf
Mike Larsson <mikelarssonftw@gmail.com>
2012-03-21 14:34:01 -0400

    updating changed css selectors in acceptance tests

--- acceptance-test/issuer-frame-test.js
+++ acceptance-test/issuer-frame-test.js
@@ -13,7 +13,7 @@ require('./acceptance-test-utils.js').createClient({
     .click("css=#welcome button.logout")
     .waitForBadgePrompt("nc.large.png", "accept")
     .waitForVisibleContent({
-      selector: '#messages div.alert-message.danger',
+      selector: '#messages div.alert.alert-error',
       content: "An error occurred when trying to add the " +
                "<em>HTML9 Fundamental</em> badge to your backpack."
     })
@@ -21,21 +21,21 @@ require('./acceptance-test-utils.js').createClient({
     .click("css=#farewell button.next")
   .open('/issuer/frame').waitForPageToLoad(8000)
     // Accept 2 badges
-    .click("css=#welcome button.next.btn.primary")
+    .click("css=#welcome button.next.btn.btn-primary")
     .waitForBadgePrompt("cc.large.png", "accept")
     .waitForBadgePrompt("by.large.png", "accept")
     .waitForVisible("css=#farewell h3.badges-many")
     .click("css=#farewell button.next")
   .open('/issuer/frame').waitForPageToLoad(8000)
     // Accept 1 badge, reject 1
-    .click("css=#welcome button.next.btn.primary")
+    .click("css=#welcome button.next.btn.btn-primary")
     .waitForBadgePrompt("cc.large.png", "reject")
     .waitForBadgePrompt("by.large.png", "accept")
     .waitForVisible("css=#farewell h3.badges-1")
     .click("css=#farewell button.next")
   .open('/issuer/frame').waitForPageToLoad(8000)
     // Reject 2 badges
-    .click("css=#welcome button.next.btn.primary")
+    .click("css=#welcome button.next.btn.btn-primary")
     .waitForBadgePrompt("cc.large.png", "reject")
     .waitForBadgePrompt("by.large.png", "reject")
     .waitForVisible("css=#farewell h3.badges-0")
