73a75979cda06358b59e46508488b9d457ed8c96
Mike Larsson <mikelarssonftw@gmail.com>
2012-03-27 14:00:04 -0400

    adding db setup to failing encoding test

--- test/portfolio-model-test.js
+++ test/portfolio-model-test.js
@@ -1,11 +1,31 @@
 var vows = require('vows')
+  , mysql = require('../lib/mysql')
   , Portfolio = require('../models/portfolio')
   , should = require('should')
 
 var UNICODE_TITLE = "てすと";
 
+var createDbFixtures = function (callback) {
+  var addUser = "INSERT INTO `user` (email) VALUES ('brian@example.com')";
+  var addBadge = "INSERT INTO `badge`"
+    + "(user_id, type, endpoint, image_path, body, body_hash)"
+    + "VALUES"
+    + "(1, 'hosted', 'http://example.com', '/dev/null', '{\"wut\":\"lol\"}', 'sha256$lol')";
+  var addGroup = "INSERT INTO `group`"
+    + "(user_id, name, badges)"
+    + "VALUES"
+    + "(1, 'group 1', '{}')";
+  mysql.client.query(addUser);
+  mysql.client.query(addBadge);
+  mysql.client.query(addGroup, callback);
+};
+
 vows.describe('Portfolio model').addBatch({
   'A portfolio': {
+    topic: function () {
+      mysql.prepareTesting();
+      createDbFixtures(this.callback);
+    },
     'with unicode characters in its title': {
       topic: function () {
         return new Portfolio({
