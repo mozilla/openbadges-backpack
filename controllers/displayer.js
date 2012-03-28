904452d8f8c42ad7920b509181abb5a7b8405634
Brian J Brennan <brianloveswords@gmail.com>
2012-03-28 11:00:12 -0400

    Shorter lines.

--- controllers/displayer.js
+++ controllers/displayer.js
@@ -12,20 +12,34 @@ function emailToUserId (request, response, next) {
   var email = obj['email'];
   
   if (!email)
-    return response.send({status: 'invalid', error: 'missing `email` parameter: Must pass in an email address to convert'}, 400)
+    return response.send({
+      status: 'invalid',
+      error: 'missing `email` parameter'
+    }, 400)
   
   User.findOne({ email: email }, function (err, user) {
     if (err) {
       logger.debug('displayer#emailToUserId: there was an error getting the user');
       logger.debug('email: ' + email);
       logger.debug('error: ' + JSON.stringify(err));
-      return response.send({status: 'error', error: 'Fatal error trying to pull user `' + email + '` from database'}, 400)
+      return response.send({
+        status: 'error',
+        error: 'error trying to pull user `' + email + '` from database'
+      }, 400)
     }
     
     if (!user)
-      return response.send({status: 'missing', error: 'Could not find a user by the email address `' + email + '`'}, 404)
+      return response.send({
+        status: 'missing',
+        error: 'Could not find a user by the email address `' + email + '`'
+      }, 404)
+    
     else 
-      return response.send({status: 'okay', email: email, userId: user.get('id')}, 200);
+      return response.send({
+        status: 'okay',
+        email: email,
+        userId: user.get('id')
+      }, 200);
   })
 }
 
