8905a47f78bcd4c7260cb9773b9674f458e81cc0
Brian J Brennan <brianloveswords@gmail.com>
2012-03-30 22:55:47 -0400

    Use :format? instead so people can use Accept headers instead of extensions.

--- app.js
+++ app.js
@@ -60,10 +60,10 @@ router(app)
   .post('/issuer/validator',          'issuer.validator')
 
   
-  .get('/displayer/:dUserId/groups.:format',          'displayer.userGroups')
-  .get('/displayer/:dUserId/group/:dGroupId:.format', 'displayer.userGroupBadges')
-  .get('/displayer/convert/email',                    'displayer.emailToUserIdView')
-  .post('/displayer/convert/email',                   'displayer.emailToUserId')
+  .get('/displayer/:dUserId/groups.:format?',          'displayer.userGroups')
+  .get('/displayer/:dUserId/group/:dGroupId:.format?', 'displayer.userGroupBadges')
+  .get('/displayer/convert/email',                     'displayer.emailToUserIdView')
+  .post('/displayer/convert/email',                    'displayer.emailToUserId')
 
   .get('/demo',                       'demo.issuer')
   .get('/demo/ballertime',            'demo.massAward')
