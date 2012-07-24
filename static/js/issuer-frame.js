07cd24fa076ccbda0163bdd97652b5ad54516425
Mike Larsson <mikelarssonftw@gmail.com>
2012-07-24 16:20:46 -0400

    Starting to clean up and reintegrate issuer frame code.

--- static/js/issuer-frame.js
+++ static/js/issuer-frame.js
@@ -11,213 +11,6 @@ jQuery.fn.extend({
   }
 });
 
-var Testing = (function setupTestingEnvironment() {
-  return;
-  if (window.parent !== window)
-    return;
-
-  var Testing = {
-    browseridWorks: true,
-    instantTransitions: function() {
-      var DELAY_MS = 10;
-      function scheduleCallback(cb, self) {
-        if (cb)
-          setTimeout(function() {
-            cb.call(self);
-          }, DELAY_MS);
-      }
-      function quickShow(cb) {
-        this.show();
-        scheduleCallback(cb, this);
-      }
-      function quickHide(cb) {
-        this.hide();
-        scheduleCallback(cb, this);
-      }
-      jQuery.fn.extend({
-        fadeIn: quickShow,
-        fadeOut: quickHide,
-        slideDown: quickShow,
-        slideUp: quickHide
-      });
-    }
-  };
-  
-  var FAKE_XHR_DELAY = 10;
-  var ASSERTIONS = [
-    "http://foo.org/newbadge.json",
-    "http://foo.org/another_newbadge.json",
-    "http://foo.org/nonexistent.json",
-    "http://bar.org/oldbadge.json",
-    "http://foo.org/makebackpackexplode.json"
-  ];
-  var RESPONSES = {
-    "http://foo.org/makebackpackexplode.json": {
-      owner: false,
-      exists: false,
-      badge: {
-        "recipient": "someone_else@example.com",
-        "evidence": "/badges/html9-basic/example",
-        "badge": {
-          "version": "0.5.0",
-          "name": "HTML9 Fundamental",
-          "image": "/_demo/nc.large.png",
-          "description": "Fetchable and validates fine client-side but not server-side",
-          "criteria": "/badges/html9-basic",
-          "issuer": {
-            "origin": "http://p2pu.org",
-            "name": "P2PU",
-            "org": "School of Webcraft",
-            "contact": "admin@p2pu.org"
-          }
-        }
-      }
-    },
-    "http://foo.org/newbadge.json": {
-      owner: true,
-      exists: false,
-      badge: {
-        "recipient": "example@example.com",
-        "evidence": "/badges/html5-basic/example",
-        "badge": {
-          "version": "0.5.0",
-          "name": "HTML5 Fundamental",
-          "image": "/_demo/by.large.png",
-          "description": "Knows the difference between a <section> and an <article>",
-          "criteria": "/badges/html5-basic",
-          "issuer": {
-            "origin": "http://p2pu.org",
-            "name": "P2PU",
-            "org": "School of Webcraft",
-            "contact": "admin@p2pu.org"
-          }
-        }
-      }
-    },
-    "http://foo.org/another_newbadge.json": {
-      owner: true,
-      exists: false,
-      badge: {
-        "recipient": "example@example.com",
-        "evidence": "/badges/html6-basic/example",
-        "badge": {
-          "version": "0.5.0",
-          "name": "HTML6 Fundamental",
-          "image": "/_demo/cc.large.png",
-          "description": "Knows the difference between a <sprite> and a <hamster>",
-          "criteria": "/badges/html6-basic",
-          "issuer": {
-            "origin": "http://p2pu.org",
-            "name": "P2PU",
-            "org": "School of Webcraft",
-            "contact": "admin@p2pu.org"
-          }
-        }
-      }
-    },
-    "http://bar.org/oldbadge.json": {
-      owner: true,
-      exists: true,
-      badge: {
-        "recipient": "example@example.com",
-        "evidence": "/badges/html4-basic/example",
-        "badge": {
-          "version": "0.5.0",
-          "name": "HTML4 Fundamental",
-          "image": "/_demo/cc.large.png",
-          "description": "Knows the difference between a <p> and an <b>",
-          "criteria": "/badges/html4-basic",
-          "issuer": {
-            "origin": "http://p2pu.org",
-            "name": "P2PU",
-            "org": "School of Webcraft",
-            "contact": "admin@p2pu.org"
-          }
-        }
-      }
-    }
-  };
-
-  function show(text) {
-    var div = $('<pre style="whitespace: pre-wrap"></pre>');
-    div.text(text);
-    $("#test-info .log").prepend(div);
-    div.hide().slideDown();
-  }
-  
-  var fakeResponseHandlers = {
-    "POST /backpack/authenticate": function(options, cb) {
-      if (Testing.browseridWorks)
-        cb(200, 'OK', {
-          json: {
-            email: options.data.assertion
-          }
-        });
-      else
-        cb(400, 'Bad Request');
-    },
-    "POST /issuer/assertion": function(options, cb) {
-      if (options.data.url == "http://foo.org/makebackpackexplode.json")
-        cb(400, 'Bad Request', {
-          text: JSON.stringify({
-            message: "blah"
-          })
-        });
-      else
-        cb(200, 'OK');
-    },
-    "GET /issuer/assertion": function(options, cb) {
-      if (options.data.url in RESPONSES) {
-        cb(200, 'OK', {json: RESPONSES[options.data.url]});
-      } else
-        cb(404, 'Not Found');
-    }
-  };
-  
-  jQuery.meta("X-Current-User", "example@example.com");
-  jQuery.ajaxTransport("+*", function(options, originalOptions, jqXHR) {
-    return {
-      send: function(headers, completeCallback) {
-        setTimeout(function() {
-          var string = options.type + " " + originalOptions.url;
-          if (string in fakeResponseHandlers) {
-            fakeResponseHandlers[string]({
-              data: originalOptions.data
-            }, completeCallback);
-          } else {
-            completeCallback(404, 'Not Found');
-          }
-          //console.log("ajax", options.type, originalOptions.url, options, originalOptions, headers);
-        }, FAKE_XHR_DELAY);
-      },
-      abort: function() {
-        throw new Error("abort() is not implemented!");
-      }
-    };
-  });
-  
-  $(window).ready(function() {
-    window.issue(ASSERTIONS, function(errors, successes) {
-      show("If this page were not in test mode, it would close now, " +
-           "and the following information would be passed back " +
-           "to the parent frame.\n\n" + 
-           "errors:\n\n" + JSON.stringify(errors, null, " ") +
-           "\n\nsuccesses:\n\n" + JSON.stringify(successes, null, " "));
-    });
-    $("#test-info").show();
-    show("The simulated assertions passed to this page are:\n\n " +
-         JSON.stringify(ASSERTIONS, null, " "));
-  });
-  if (!navigator.id)
-    navigator.id = {};
-  navigator.id.getVerifiedEmail = function(cb) {
-    var email = "someone_else@example.com";
-    show("We just simulated a BrowserID login of " + email + ".");
-    cb(email);
-  };  
-  return Testing;
-})();
-
 var Session = Session();
 var App;
 
@@ -274,159 +67,6 @@ function showError(template, data) {
   });
 }
 
-// This is the core issuing implementation; the response is proxied
-// back to the parent window. The function is global so it can be
-// overridden from testing suites.
-function issue(assertions, cb) {
-  if (assertions.length == 1) {
-    $("#welcome .badge-count").text("1 badge");
-  }
-  else {
-    $("#welcome .badge-count").text(assertions.length + " badges");
-  }
-  $("#welcome").fadeIn();
-  var errors = [];
-  var successes = [];
-  function exit() {
-    // We're on our way out. Disable all event handlers on the page,
-    // so the user can't do anything.
-    $("button, a").unbind();
-    cb(errors, successes);
-  }
-  
-  // setup key handler so we exit immediately if the user hits escape
-  $('body').keydown(function (event) {
-    if (event.keyCode === 27) exit();
-  });
-  
-  window.Assertions = {
-    processNext: function() {
-      if (assertions.length == 0) {
-        if ($("#welcome:visible").length) {
-          exit();
-          return;
-        }
-        if (successes.length < 2)
-          $("#farewell .badges-" + successes.length).show();
-        else {
-          $("#farewell .badges-many").show();
-          $("#farewell .badges-added").text(successes.length);
-        }
-        $("#farewell .next").click(exit);
-        $(".navbar .closeFrame").unbind().click(exit);
-        $("#farewell").fadeIn();
-        return;
-      }
-      var url = assertions[assertions.length-1];
-      // TODO: parse the URL to see if it's malformed.
-      jQuery.ajax({
-        url: '/issuer/assertion',
-        data: {
-          url: url
-        },
-        success: function(obj) {
-          var templateArgs = {
-            hostname: url,
-            assertion: obj.badge,
-            recipient: obj.recipient,
-            user: Session.currentUser
-          };
-          // if the badge already exists in the database, skip this assertion
-          if (obj.exists) {
-            assertions.pop();
-            errors.push({
-              url: url,
-              reason: 'EXISTS'
-            });
-            showError("#already-exists-template", templateArgs);
-            processNext();
-          }
-
-          // make sure the recipient matches the current user.
-          else if (!obj.owner) {
-            assertions.pop();
-            errors.push({
-              url: url,
-              reason: 'INVALID'
-            });
-            showError("#owner-mismatch-template", templateArgs);
-            processNext();
-          }
-
-          else {
-            $("#badge-ask").empty()
-              .append($("#badge-ask-template").render(templateArgs)).fadeIn();
-            $("#badge-ask .accept").click(function() {
-              assertions.pop();
-              var post = jQuery.ajax({
-                type: 'POST',
-                url: '/issuer/assertion',
-                data: {
-                  url: url
-                },
-                success: function(data, textStatus, jqXHR) {
-                  if (jqXHR.status == 304) {
-                    showError("#already-exists-template", templateArgs);
-                    errors.push({
-                      url: url,
-                      reason: "EXISTS"
-                    });
-                  } else
-                    successes.push(url);
-                },
-                error: function(req) {
-                  showError("#accept-failure-template", templateArgs);
-                  errors.push({
-                    url: url,
-                    // TODO: Is this really the best reason?
-                    reason: "INVALID"
-                  });
-                }
-              });
-              var fade = jQuery.Deferred();
-              $("#badge-ask").fadeOut(function() { fade.resolve(); });
-              jQuery.when(fade, post).always(processNext);
-            });
-            $("#badge-ask .reject").click(function() {
-              assertions.pop();
-              errors.push({
-                url: url,
-                reason: 'DENIED'
-              });
-              $("#badge-ask").fadeOut(processNext);
-            });
-          }
-        },
-        error: function(xhr) {
-          assertions.pop();
-          var templateArgs = {
-            error : jQuery.parseJSON(xhr.responseText)
-          };
-          errors.push({
-            url: url,
-            reason: 'INACCESSIBLE',
-            message: templateArgs.error.message
-          });
-          showError("#inaccessible-template", templateArgs);
-          processNext();
-        }
-      });
-    }
-  };
-  var processNext = window.Assertions.processNext;
-  $(".navbar .closeFrame").click(function() {
-    assertions.forEach(function(assertion) {
-      errors.push({
-        url: assertion,
-        reason: 'DENIED'
-      });
-    });
-    assertions = [];
-    exit();
-    return false;
-  });
-}
-
 function issue(assertions, cb){
 
   if (assertions.length == 1) {
@@ -437,55 +77,7 @@ function issue(assertions, cb){
   }
   $("#welcome").fadeIn();
 
-  App = App(assertions, {
-    build: function(assertion){
-      var d = $.Deferred();
-      jQuery.ajax({
-	url: '/issuer/assertion',
-	data: {
-	  url: assertion
-	},
-	success: function(obj){
-	  if (obj.exists && false) {
-	    d.reject('EXISTS');
-	  }
-	  else if (!obj.owner) {
-	    d.reject('INVALID');
-	  }
-	  else {
-	    d.resolve(obj.badge);
-	  }
-	},
-	error: function(err){
-	  d.reject('INACCESSIBLE');
-	}
-      });
-      return d;
-    },
-    issue: function(assertion){
-      var d = $.Deferred();
-      var self = this;
-      var post = jQuery.ajax({
-	type: 'POST',
-	url: '/issuer/assertion',
-	data: {
-	  url: assertion
-	},
-	success: function(data, textStatus, jqXHR) {
-	  if (jqXHR.status == 304) {
-	    d.reject('EXISTS');
-	  } else
-	    successes.push(url);
-	    d.resolve();
-	},
-	error: function(req) {
-	  // TODO
-	}
-      });
-      return d;
-    }
-  });
-
+  App = App(assertions);
   var badgesProcessed;
 
   App.on('badges-ready', function(failures, badges){
@@ -500,9 +92,8 @@ function issue(assertions, cb){
 	return;
       }
 
-      var badge = badges[next];
-      next++;
-      console.log('offering', badge.assertion);
+      // TODO: clean up the data model/terminology below
+      var badge = badges[next++];
       var obj = badge.badgeData();
       var templateArgs = {
 	hostname: badge.assertion,
@@ -521,18 +112,17 @@ function issue(assertions, cb){
 	offerNext();
       });
     }
-    console.log('begin offering');
     offerNext();
   });
 
+  function exit(failures, successes) {
+    // We're on our way out. Disable all event handlers on the page,
+    // so the user can't do anything.
+    $("button, a").unbind();
+    cb(failures, successes);
+  }
+
   App.on('badges-complete', function(failures, successes, t){
-    console.log('failed', failures, 'succeeded', successes);
-    function exit() {
-      // We're on our way out. Disable all event handlers on the page,
-      // so the user can't do anything.
-      $("button, a").unbind();
-      cb(failures, successes);
-    }
     $.when(badgesProcessed).always(function(){
       if (successes.length < 2)
 	$("#farewell .badges-" + successes.length).show();
@@ -540,20 +130,15 @@ function issue(assertions, cb){
 	$("#farewell .badges-many").show();
 	$("#farewell .badges-added").text(successes.length);
       }
-      $("#farewell .next").click(exit);
-      $(".navbar .closeFrame").unbind().click(exit);
+      $("#farewell .next").click(function(){ exit(failures, successes); });
+      $(".navbar .closeFrame").unbind().click(function(){ exit(failures, successes); });
       $("#farewell").fadeIn();
       return;
     });
   });
 
   App.on('badges-aborted', function(failures, successes, t){
-    $("button, a").unbind();
-    cb(failures, successes);
-  });
-
-  App.on('state-change', function(badge, state){
-    console.log('state-change', badge, state);
+    exit(failures, successes);
   });
 
   $(".navbar .closeFrame").click(function() {
