ecdc79f60dd07559962c3dac66d58186c7ae96cb
Brian J Brennan <brianloveswords@gmail.com>
2012-08-10 16:20:30 -0400

    Ensure prereqs before making DB.

--- .puppet-manifests/openbadges.pp
+++ .puppet-manifests/openbadges.pp
@@ -3,6 +3,7 @@ class openbadges::db {
     exec { "create-${name}-db":
       unless => "/usr/bin/mysql -u${user} -p${password} ${name}",
       command => "/usr/bin/mysql -uroot -p$mysql_password -e \"create database ${name} character set ${encoding}; grant all on ${name}.* to ${user}@localhost identified by '$password';\"",
+      require => [ Package['mysql-server'], Package['mysql-client'] ],
     }
   }
   mysqldb { "openbadges":
