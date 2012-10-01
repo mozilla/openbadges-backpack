621bcf4a29b4c47e5f36ee1138ec49a35d5f82f6
Mike Larsson <mikelarssonftw@gmail.com>
2012-10-01 10:24:39 -0400

    Converting issuer.hogan.js to jinja2, updating demo controller to Express 3

--- controllers/demo.js
+++ controllers/demo.js
@@ -14,7 +14,7 @@ var ORIGIN = protocol + '://' + configuration.get('hostname') + (port ? ':' + po
 
 // Render the view for the demo badge issuer.
 exports.issuer = function (req, res) {
-  res.render('issuer', {
+  res.render('issuer.html', {
     login: false,
     title: 'Demo Issuer',
     csrfToken: req.session._csrf
@@ -27,7 +27,8 @@ exports.award = function (req, res) {
   var bakeURL = ORIGIN + '/baker?award=true&assertion=' + assertionURL;
 
   request({url: bakeURL, encoding: 'binary'},  function (err, resp, body) {
-    res.send(Buffer(body, 'binary'), {'content-type': 'image/png'});
+    res.set('Content-Type', 'image/png');
+    res.send(Buffer(body, 'binary'));
   });
 };
 
