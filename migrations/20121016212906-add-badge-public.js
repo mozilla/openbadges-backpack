42ad9e48b0d615516e5450461cb41665e817aa27
Alan Kligman <alan.kligman@gmail.com>
2012-10-16 17:28:06 -0400

    Added migration for new public column in badge table.

---
+++ migrations/20121016212906-add-badge-public.js
@@ -0,0 +1,16 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  var query = "ALTER TABLE badge ADD public BOOLEAN DEFAULT false";
+  db.all(query, function(err, results) {
+    callback();
+  });
+};
+
+exports.down = function(db, callback) {
+  var query = "ALTER TABLE badge DROP public";
+  db.all(query, function(err, results) {
+    callback();
+  });
+};
