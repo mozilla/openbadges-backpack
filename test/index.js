81fc1e8b7a80a51d7749da5d80c5b27cc7808124
Brian J Brennan <brianloveswords@gmail.com>
2012-11-15 15:59:26 -0500

    Implement fixture feature of prepareDatabase

--- test/index.js
+++ test/index.js
@@ -1,6 +1,12 @@
+const _ = require('underscore');
 const mysql = require('../lib/mysql');
 const test = require('tap').test;
 const async = require('async');
+
+/**
+ * Make a cleanup test that closes the database connection
+ *
+ */
 
 test.closeDatabase = function closeDatabase () {
   test('closing database', function (t) {
@@ -8,8 +14,32 @@ test.closeDatabase = function closeDatabase () {
   });
 };
 
-test.prepareDatabase = function prepareDatabase(callback) {
-  callback = callback || function(){}
+/**
+ * Recreate the test database and optionally insert test data.
+ *
+ * @asyncronous
+ * @return {Object} whatever fixtures were passed into it
+ */
+
+test.prepareDatabase = function prepareDatabase(fixtures, callback) {
+  if (typeof fixtures === 'function')
+    callback = fixtures, fixtures = {};
+  callback = callback || function(){};
+
+  const putFixtures = insertFixtures.bind(null, fixtures);
+
+  async.series([
+    recreateDatabase,
+    putFixtures,
+  ], function (err, results) {
+    if (err) throw err;
+    callback(results[1]);
+  });
+};
+
+module.exports = test;
+
+function recreateDatabase(callback) {
   async.series([
     mysql.dropTestDatabase,
     mysql.createTestDatabase,
@@ -17,8 +47,28 @@ test.prepareDatabase = function prepareDatabase(callback) {
     mysql.createTables
   ], function (err, results) {
     if (err) throw err;
-    callback(test.closeDatabase);
+    callback(null, results);
   });
-};
+}
 
-module.exports = test;
+function keyFromObj(obj) {
+  return function (key) { return obj[key] };
+}
+
+function sortedValues(obj) {
+  return _.keys(obj).sort().map(keyFromObj(obj));
+}
+
+function itemSaver(item, callback) {
+  console.dir(callback);
+  return item.save(callback);
+}
+
+function insertFixtures(fixtures, callback) {
+  var items = sortedValues(fixtures);
+  async.mapSeries(items, itemSaver, function (err, results) {
+    if (err) throw err;
+    callback(null, fixtures);
+  })
+}
+
