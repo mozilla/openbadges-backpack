33ccaac982c2de4b74f9ceae49ea0fa14891cec9
Brian J Brennan <brianloveswords@gmail.com>
2012-11-29 17:07:43 -0500

    Ensure group being pulled belongs to user.
    In the displayer API, we were neglecting to check if the group actually
    belonged to the specified user. While this isn't necessarily a security
    hole   groups would still have to be marked as public to be shown   it
    was certainly less than ideal.

--- controllers/displayer.js
+++ controllers/displayer.js
@@ -7,46 +7,77 @@ var logger = require('../lib/logging').logger;
 
 // Helpers
 // -------
-var formatters = {
+var FORMATTERS = {
   'application/json': function (responseData, request) {
     var callback = request.query.callback;
     if (!callback)
       return responseData;
-    else
-      return callback + '(' + JSON.stringify(responseData) + ')';
+    return callback + '(' + JSON.stringify(responseData) + ')';
   }
 };
 
-function formatResponse(data, request, response) {
-  var rawData = data;
-  var status = data.httpStatus;
-  var jsonp = request.query.callback;
-  var accept = request.headers['accept'];
-  var url = request.url;
 
-  // accept header is secondary as it will be less often used
+/**
+ * Determine the requested format by either accept header or extension.
+ * If a format cannot be determined, defaults to "application/json".
+ *
+ * @param {Request} req The incoming request object
+ * @return {String} requested format.
+ */
+
+function determineFormat(req) {
+  var accept = req.headers['accept'];
+  var url = req.url.split('?')[0]
   var format = accept || 'application/json';
-
-  // remove the query string
-  url = url.split('?')[0];
-
-  // extensions rule everything around me.
   if (url.match(/\.json(p?)$/))
     format = 'application/json';
-
-  // if we can't find a formatter, send out a plain text response.
-  if (!formatters[format]) {
-    logger.debug('could not find a formatter for api request');
-    logger.debug('  url: ' + url);
-    logger.debug('  accept header: ' + accept);
-    response.contentType('txt');
-    return response.send('error: could not find formatter', 400);
-  }
-
-  response.setHeader('Content-Type', format);
-  var preparedData = formatters[format](rawData, request);
-  return response.send(preparedData, jsonp ? 200 : status);
+  return format;
 }
+
+/**
+ * Log errors with the formatter.
+ *
+ * @param {Request} req The incoming request object
+ */
+
+function logFormatterError(req) {
+  logger.debug('could not find a formatter for api request');
+  logger.debug('  url: ' + req.url);
+  logger.debug('  accept header: ' + req.headers['accept']);
+}
+
+/**
+ * Send out a response in the format requested by the user-agent.
+ *
+ * @param {Object} data The object to respond with
+ * @param {Request} req The incoming HTTP request object
+ * @param {Response} res The HTTP response object
+ */
+
+function formatResponse(data, req, res) {
+  var jsonp = req.query.callback;
+  var status = jsonp
+    ? 200
+    : (data.httpStatus || 200);
+  var format = determineFormat(req);
+  var formatter = FORMATTERS[format];
+  var responseBody;
+  if (!formatter) {
+    logFormatterError(req);
+    res.type('txt');
+    return res.send(400, 'error: could not find formatter');
+  }
+  responseBody = formatter(data, req);
+  res.type(format);
+  return res.send(status, responseBody);
+}
+
+/**
+ * Fully qualify a relative URL using entries from the configuration
+ *
+ * @param {String} pathname
+ * @return A fully qualified URL.
+ */
 
 function fullUrl(pathname) {
   return require('url').format({
@@ -60,111 +91,118 @@ function fullUrl(pathname) {
 // Parameter Handlers
 // ------------------
 
-// make sure to always return 200 to jsonp or else it will fail
-// silently on the client -- the user agent won't bother to make
-// a script tag if the resource is "not found".
-function findThing(name) {
-  var M = ({ user: User, group: Group })[name];
-  return function thingFinder(request, response, next, id) {
-    var jsonp = request.query.callback;
+/**
+ * (Param Handler) Find a group by its ID
+ */
 
-    M.findById(id, function (err, thing) {
-      if (err) {
-        logger.error("Error pulling " + name + ": " + err);
-        return formatResponse({
-          httpStatus: 500,
-          status: 'error',
-          error: 'Could not pull ' + name
-        }, request, response);
-      }
-
-      if (!thing || !thing.get('id'))
-        return formatResponse({
-          httpStatus: 404,
-          status: 'missing',
-          error: 'Could not find ' + name
-        }, request, response);
-
-      if (request[name]) request['_' + name] = request[name];
-      request[name] = thing;
-      return next();
-    });
-  };
-}
-
-exports.param = {
-  dUserId: findThing('user'),
-  dGroupId: findThing('group')
+exports.findGroupById = function findGroupById(req, res, next, id) {
+  console.log('finding group');
+  Group.findById(id, function (err, group) {
+    if (err)
+      return next(err);
+    if (!group)
+      return formatResponse({
+        httpStatus: 404,
+        status: 'missing',
+      }, req, res);
+    req.group = group;
+    return next();
+  });
 };
+
+/**
+ * (Param Handler) Find a user by its ID
+ */
+
+exports.findUserById = function findUserById(req, res, next, id) {
+  console.log('finding user');
+  User.findById(id, function (err, user) {
+    if (err)
+      return next(err);
+    if (!user)
+      return formatResponse({
+        httpStatus: 404,
+        status: 'missing',
+      }, req, res);
+    req.user = user;
+    return next();
+  });
+};
+
 
 // Controllers
 // ------------
-function displayerAPIVersion(request, response, next) {
-  return formatResponse({
-    status: 'okay',
-    version: '0.5.0'
-  }, request, response);
-}
 
-function emailToUserIdView(request, response, next) {
-  return response.render('email-converter.html');
-}
+/**
+ * Show form for converting from email to userID
+ */
 
-function emailToUserId(request, response, next) {
+exports.emailToUserIdView = function emailToUserIdView(req, res, next) {
+  return res.render('email-converter.html');
+};
+
+
+/**
+ * Convert an email address to a userID.
+ */
+
+exports.emailToUserId = function emailToUserId(req, res, next) {
   // don't use formatter here -- we aren't supporting jsonp or CORS for
   // the email to userId API because we want to discourage people including
   // email addresses in cleartext (such as in the source of some javascript)
-  var obj = request.body || {};
+  var obj = req.body || {};
   var email = obj['email'];
 
   if (!email)
-    return response.send({
+    return res.send(400, {
       status: 'invalid',
       error: 'missing `email` parameter'
-    }, 400);
+    });
 
   User.findOne({ email: email }, function (err, user) {
     if (err) {
       logger.debug('displayer#emailToUserId: there was an error getting the user');
       logger.debug('email: ' + email);
       logger.debug('error: ' + JSON.stringify(err));
-      return response.send({
+      return res.send(400, {
         status: 'error',
         error: 'error trying to pull user `' + email + '` from database'
-      }, 400);
+      });
     }
 
-    if (!user)
-      return response.send({
+    if (!user) {
+      return res.send(404, {
         status: 'missing',
         error: 'Could not find a user by the email address `' + email + '`'
-      }, 404);
+      });
+    }
 
-    else
-      return response.send({
-        status: 'okay',
-        email: email,
-        userId: user.get('id')
-      }, 200);
+    return res.send(200, {
+      status: 'okay',
+      email: email,
+      userId: user.get('id')
+    });
   });
-}
+};
 
-function userGroups(request, response, next) {
-  var user = request.user;
-  var jsonp = request.query.callback;
-  if (!user || !user.get('id'))
-    // make sure to always return 200 to jsonp or else it will fail
-    // silently on the client -- the user agent won't bother to make
-    // a script tag if the resource is "not found".
-    return formatResponse({
-      httpStatus: 404,
-      status: 'missing',
-      error: 'Could not find user'
-    }, request, response);
 
+/**
+ * Get all of the public groups for a user. Only lists group IDs, not
+ * the badges in the group.
+ *
+ * This is expected to come after some middleware that either sets
+ * `req.user` or 404s.
+ *
+ * @see `findUserById`
+ */
+
+exports.userGroups = function userGroups(req, res, next) {
+  var user = req.user;
+  var jsonp = req.query.callback;
   var userId = user.get('id');
+  var query = { user_id: userId, 'public': 1 };
 
-  Group.find({ user_id: userId, 'public': 1 }, function (err, groups) {
+  Group.find(query, function (err, groups) {
     var exposedGroupData = _.map(groups, function (group) {
       return {
         groupId: group.get('id'),
@@ -176,22 +214,26 @@ function userGroups(request, response, next) {
     return formatResponse({
       userId: userId,
       groups: exposedGroupData
-    }, request, response);
+    }, req, res);
   });
-}
+};
 
-function userGroupBadges(request, response, next) {
-  var user = request.user;
-  var group = request.group;
-  var jsonp = request.query.callback;
+/**
+ * Get all of the badges within a specific group.
+ */
 
-  // if the group exists but it's private, just pretend it doesn't exist
-  if (!group.get('public'))
+exports.userGroupBadges = function userGroupBadges(req, res, next) {
+  var user = req.user;
+  var group = req.group;
+  var userOwnsGroup = (user.get('id') === group.get('user_id'));
+  var groupIsPublic = !!group.get('public');
+
+  if (!userOwnsGroup || !groupIsPublic) {
     return formatResponse({
       httpStatus: 404,
-      status: 'missing',
-      error: 'Could not find group'
-    }, request, response);
+      status: 'missing'
+    }, req, res);
+  }
 
   group.getBadgeObjects(function (err, badges) {
     // #TODO: revisit this when we implement signed badges
@@ -208,12 +250,6 @@ function userGroupBadges(request, response, next) {
       userId: user.get('id'),
       groupId: group.get('id'),
       badges: exposedBadgeData
-    }, request, response);
+    }, req, res);
   });
-}
-
-exports.version = displayerAPIVersion;
-exports.emailToUserId = emailToUserId;
-exports.emailToUserIdView = emailToUserIdView;
-exports.userGroups = userGroups;
-exports.userGroupBadges = userGroupBadges;
+};
