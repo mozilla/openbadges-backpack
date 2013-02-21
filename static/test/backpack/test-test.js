181b70fa343f81b39c69442c43e3b34fb7fb71a2
Atul Varma <varmaa@gmail.com>
2013-02-21 15:56:21 -0500

    added Badge.isExpired().

--- static/test/backpack/test-test.js
+++ static/test/backpack/test-test.js
@@ -1,11 +1,26 @@
 "use strict";
 
-defineTests([], function(){
+defineTests([], function() {
+  var MS_PER_DAY = 1000 * 60 * 60 * 24;
+  var formatDate = function(date) {
+    return date.getFullYear() + '-' +
+           (date.getMonth() + 1) + '-' +
+           date.getDate();
+  };
 
-  module('Delete me');
+  module('Backpack');
 
-  test('foo', function(){
-    ok(1);
+  test('Badge.isExpired() returns false', function(){
+    var badge = new Badge.Model();
+    var tomorrow = new Date(Date.now() + MS_PER_DAY);
+    badge.attributes.body = {expires: formatDate(tomorrow)};
+    equal(badge.isExpired(), false);
   });
 
+  test('Badge.isExpired() returns true', function(){
+    var badge = new Badge.Model();
+    var yesterday = new Date(Date.now() - MS_PER_DAY);
+    badge.attributes.body = {expires: formatDate(yesterday)};
+    equal(badge.isExpired(), true);
+  });
 });
