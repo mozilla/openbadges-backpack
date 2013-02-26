06fb0cdf4dc99e487fe6393cfd1f817f9125efc2
Atul Varma <varmaa@gmail.com>
2013-02-26 11:02:04 -0500

    Added controller for backpack connect.
    This controller is fairly generic/configurable, and can be used in the
    future w/ any kind of service that wants to offer what effectively
    amounts to a sort of "federated oauth".

---
+++ controllers/backpack-connect.js
@@ -0,0 +1,128 @@
+var querystring = require('querystring');
+
+var logger = require('../lib/logging').logger;
+
+var BackpackConnect = module.exports = function BackpackConnect(options) {
+  this.Model = options.Model ||
+               require('../models/backpack-connect').Session;
+  this.apiRoot = options.apiRoot;
+  this.realm = options.realm;
+};
+
+BackpackConnect.prototype = {
+  refresh: function() { return refresh.bind(this); },
+  allowAccess: function() { return allowAccess.bind(this); },
+  authorize: function() { return authorize.bind(this); }
+};
+
+function refresh(req, res) {
+  if (!req.body)
+    return res.send('body expected', 400);
+  if (req.body.grant_type != "refresh_token")
+    return res.send('invalid grant_type', 400);
+  
+  var refresh_token = req.body.refresh_token || '';
+  
+  this.Model.find({refresh_token: refresh_token}, function(err, results) {
+    var session = results && results[0];
+    if (err) {
+      logger.warn('There was an error looking up a refresh token');
+      logger.debug(err);
+      return res.send(500);
+    }
+    if (session) {
+      session.refresh();
+      session.save(function(err) {
+        if (err) {
+          logger.warn('There was an error saving a refreshed token');
+          logger.debug(err);
+          return res.send(500);
+        }
+        return res.send({
+          expires: session.tokenLifetime,
+          access_token: session.get('access_token'),
+          refresh_token: session.get('refresh_token')
+        });
+      });
+    } else {
+      return res.send('invalid refresh_token', 400);
+    }
+  });
+}
+
+function allowAccess(req, res) {
+  if (!req.user)
+    return res.send(403);
+  if (!req.body)
+    return res.send('body expected', 400);
+  if (!req.body.callback)
+    return res.send('callback expected', 400);
+  if (!req.body.scope)
+    return res.send('scope expected', 400);
+  
+  var originErr = this.Model.validators.origin(req.body.callback);
+  var scopes = req.body.scope.split(',');
+  var scopeErr = this.Model.validators.permissions(scopes);
+  var apiRoot = this.apiRoot;
+  var session;
+  
+  if (originErr)
+    return res.send('invalid callback: ' + originErr, 400);
+  if (scopeErr)
+    return res.send('invalid scope: ' + scopeErr, 400);
+
+  session = new this.Model({
+    origin: req.body.callback,
+    user_id: req.user.get('id'),
+    permissions: scopes
+  });
+  session.save(function(err) {
+    if (err) {
+      logger.warn('There was an error creating a backpack connect token');
+      logger.debug(err);
+      return res.send(500);
+    }
+    return res.redirect(req.body.callback + "?" + querystring.stringify({
+      access_token: session.get('access_token'),
+      refresh_token: session.get('refresh_token'),
+      expires: session.tokenLifetime,
+      api_root: apiRoot
+    }), 303);
+  });
+}
+
+function authorize(req, res, next) {
+  var auth = (req.headers['authorization'] || '').match(/^Bearer (.+)$/);
+  var bearerRealmStr = 'Bearer realm="' + this.realm + '"';
+  var invalidTokenError = function(desc) {
+    // For more information on this, see:
+    // http://tools.ietf.org/html/rfc6750#section-3.1
+    res.header('WWW-Authenticate', [
+      bearerRealmStr,
+      'error="invalid_token"',
+      'error_description="' + desc + '"'
+    ].join(', '));
+    return res.send(401);
+  };
+
+  if (!auth) {
+    res.header('WWW-Authenticate', bearerRealmStr);
+    return res.send("access token expected", 401);
+  }
+  
+  this.Model.find({access_token: auth[1]}, function(err, results) {
+    if (err) {
+      logger.warn('There was an error retrieving an access token');
+      logger.debug(err);
+      return res.send(500);
+    }
+    if (results.length) {
+      if (results[0].isExpired())
+        return invalidTokenError("The access token expired");
+      req.backpackConnect = results[0];
+      return next();
+    } else {
+      return invalidTokenError("Unknown access token");
+    }
+  });
+}
---
+++ test/backpack-connect-controller.test.js
@@ -0,0 +1,283 @@
+const url = require('url');
+
+const test = require('tap').test;
+const testUtils = require('./');
+const conmock = require('./conmock');
+
+const BPCSession = require('../models/backpack-connect').SessionFactory({
+  now: function() { return nowSecs * 1000; },
+  tokenLifetime: 1000,
+  uid: function() { return "SOME_UID_" + (++uid); },
+  validPermissions: ["foo_perm"]
+});
+const User = require('../models/user');
+const BackpackConnect = require('../controllers/backpack-connect');
+const bpc = new BackpackConnect({
+  Model: BPCSession,
+  apiRoot: 'http://backpack.org/test_api',
+  realm: 'test'
+});
+
+var uid = 0;
+var nowSecs = 0;
+
+testUtils.prepareDatabase({
+  '1-real-user': new User({ email: 'brian@example.org' }),
+}, function (fixtures) {
+  test('refresh() fails w/ no body', function(t) {
+    conmock({handler: bpc.refresh()}, function(err, mock) {
+      t.equal(mock.status, 400);
+      t.same(mock.body, "body expected");
+      t.end();
+    });
+  });
+
+  test('refresh() fails w/ bad grant_type', function(t) {
+    conmock({
+      handler: bpc.refresh(),
+      request: {
+        body: {
+          grant_type: "something_weird"
+        }
+      }
+    }, function(err, mock) {
+      t.equal(mock.status, 400);
+      t.same(mock.body, "invalid grant_type");
+      t.end();
+    });
+  });
+
+  test('refresh() fails w/ empty token', function(t) {
+    conmock({
+      handler: bpc.refresh(),
+      request: {
+        body: {
+          grant_type: "refresh_token",
+          refresh_token: ""
+        }
+      }
+    }, function(err, mock) {
+      t.equal(mock.status, 400);
+      t.same(mock.body, "invalid refresh_token");
+      t.end();
+    });
+  });
+  
+  test('refresh() fails w/ bad token', function(t) {
+    conmock({
+      handler: bpc.refresh(),
+      request: {
+        body: {
+          grant_type: "refresh_token",
+          refresh_token: "LOL"
+        }
+      }
+    }, function(err, mock) {
+      t.equal(mock.status, 400);
+      t.same(mock.body, "invalid refresh_token");
+      t.end();
+    });
+  });
+  
+  test('allowAccess() 403\'s when unauthenticated', function(t) {
+    conmock({handler: bpc.allowAccess()}, function(err, mock) {
+      if (err) throw err;
+      t.equal(mock.status, 403);
+      t.end();
+    });
+  });
+
+  test('allowAccess() fails w/ no body', function(t) {
+    conmock({
+      handler: bpc.allowAccess(),
+      request: {user: fixtures['1-real-user']}
+    }, function(err, mock) {
+      if (err) throw err;
+      t.equal(mock.status, 400);
+      t.equal(mock.body, 'body expected');
+      t.end();
+    });
+  });
+
+  test('allowAccess() fails w/ empty callback', function(t) {
+    conmock({
+      handler: bpc.allowAccess(),
+      request: {
+        user: fixtures['1-real-user'],
+        body: {callback: ''}
+      }
+    }, function(err, mock) {
+      if (err) throw err;
+      t.equal(mock.status, 400);
+      t.equal(mock.body, 'callback expected');
+      t.end();
+    });
+  });
+
+  test('allowAccess() fails w/ empty scope', function(t) {
+    conmock({
+      handler: bpc.allowAccess(),
+      request: {
+        user: fixtures['1-real-user'],
+        body: {callback: 'http://foo.org', scope: ''}
+      }
+    }, function(err, mock) {
+      if (err) throw err;
+      t.equal(mock.status, 400);
+      t.equal(mock.body, 'scope expected');
+      t.end();
+    });
+  });
+
+  test('allowAccess() fails w/ bad callback', function(t) {
+    conmock({
+      handler: bpc.allowAccess(),
+      request: {
+        user: fixtures['1-real-user'],
+        body: {callback: 'LOL', scope: 'foo_perm'}
+      }
+    }, function(err, mock) {
+      if (err) throw err;
+      t.equal(mock.status, 400);
+      t.equal(mock.body, 'invalid callback: invalid origin protocol');
+      t.end();
+    });
+  });
+
+  test('allowAccess() fails w/ bad scope', function(t) {
+    conmock({
+      handler: bpc.allowAccess(),
+      request: {
+        user: fixtures['1-real-user'],
+        body: {callback: 'http://foo.org', scope: 'BAD'}
+      }
+    }, function(err, mock) {
+      if (err) throw err;
+      t.equal(mock.status, 400);
+      t.equal(mock.body, 'invalid scope: invalid permission(s): BAD');
+      t.end();
+    });
+  });
+  
+  test('authorize() fails w/ no token', function(t) {
+    conmock({handler: bpc.authorize()}, function(err, mock) {
+      t.equal(mock.status, 401);
+      t.equal(mock.header('WWW-Authenticate'), 'Bearer realm="test"');
+      t.end();
+    });
+  });
+
+  test('authorize() fails w/ invalid token', function(t) {
+    conmock({
+      handler: bpc.authorize(),
+      request: {
+        headers: {
+          'authorization': 'Bearer INVALID_ACCESS_TOKEN'
+        }
+      }
+    }, function(err, mock) {
+      t.equal(mock.status, 401);
+      t.equal(mock.header('WWW-Authenticate'),
+              'Bearer realm="test", error="invalid_token", ' +
+              'error_description="Unknown access token"');
+      t.end();
+    });
+  });
+
+  test('allowAccess() redirects properly', function(t) {
+    conmock({
+      handler: bpc.allowAccess(),
+      request: {
+        user: fixtures['1-real-user'],
+        body: {
+          callback: 'http://foo.org/callback',
+          scope: 'foo_perm'
+        }
+      }
+    }, function(err, mock) {
+      var parsed = url.parse(mock.path, true, true);
+      if (err) throw err;
+      t.equal(mock.status, 303);
+      t.equal(mock.fntype, 'redirect');
+      t.equal(parsed.protocol, "http:");
+      t.equal(parsed.host, "foo.org");
+      t.equal(parsed.pathname, "/callback");
+      t.same(parsed.query, {
+        expires: "1000",
+        api_root: "http://backpack.org/test_api",
+        access_token: "SOME_UID_1",
+        refresh_token: "SOME_UID_2"
+      });
+      t.end();
+    });
+  });
+
+  test('authorize() works w/ valid token', function(t) {
+    conmock({
+      handler: bpc.authorize(),
+      request: {
+        headers: {
+          'authorization': 'Bearer SOME_UID_1'
+        }
+      }
+    }, function(err, mock) {
+      t.equal(mock.fntype, 'next');
+      t.end();
+    });
+  });
+
+  test('authorize() fails w/ expired token', function(t) {
+    nowSecs += 1001;
+    conmock({
+      handler: bpc.authorize(),
+      request: {
+        headers: {
+          'authorization': 'Bearer SOME_UID_1'
+        }
+      }
+    }, function(err, mock) {
+      t.equal(mock.status, 401);
+      t.equal(mock.header('WWW-Authenticate'),
+              'Bearer realm="test", error="invalid_token", ' +
+              'error_description="The access token expired"');
+      t.end();
+    });
+  });
+
+  test('refresh() works', function(t) {
+    conmock({
+      handler: bpc.refresh(),
+      request: {
+        body: {
+          grant_type: "refresh_token",
+          refresh_token: "SOME_UID_2"
+        }
+      }
+    }, function(err, mock) {
+      t.equal(mock.status, 200);
+      t.same(mock.body, {
+        expires: 1000,
+        access_token: "SOME_UID_3",
+        refresh_token: "SOME_UID_4"
+      });
+      t.end();
+    });
+  });
+
+  test('authorize() works w/ refreshed token', function(t) {
+    conmock({
+      handler: bpc.authorize(),
+      request: {
+        headers: {
+          'authorization': 'Bearer SOME_UID_3'
+        }
+      }
+    }, function(err, mock) {
+      t.equal(mock.fntype, 'next');
+      t.end();
+    });
+  });
+  
+  testUtils.finish(test);
+});
+
