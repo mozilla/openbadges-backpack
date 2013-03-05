fed7cba40c446eab44d116f71afa2ca5ea804061
Brian J Brennan <brianloveswords@gmail.com>
2013-03-05 14:24:58 -0500

    Remove old validation tests.
    I want to start from scratch with the tests so I can get a better
    implementation that caters to the new validator rather than trying to
    shoehorn the new validator into the old validator output.

--- test/issuer-controller.test.js
+++ test/issuer-controller.test.js
@@ -18,54 +18,7 @@ testUtils.prepareDatabase({
     body: testUtils.makeAssertion({})
   }),
 }, function (fixtures) {
-  test('issuer#validator: render when given no body', function (t) {
-    conmock(issuer.validator, function (err, mock) {
-      t.same(mock.status, 200);
-      t.same(mock.fntype, 'render');
-      t.end();
-    });
-  });
-
-  test('issuer#validator: reporting errors', function (t) {
-    const expect = [
-      'recipient',
-      'badge.version',
-      'badge.name',
-      'badge.description',
-      'badge.criteria',
-      'badge.image',
-      'badge.issuer.origin',
-      'badge.issuer.name'
-    ].sort();
-
-    t.test('html style', function (t) {
-      conmock({
-        handler: issuer.validator,
-        request: {body: { assertion: '{}' }}
-      }, function (err, mock) {
-        const errors = _.pluck(mock.options.errors, 'field').sort();
-        t.same(mock.fntype, 'render', 'should be a render call');
-        t.same(errors, expect, 'should get expected errors');
-        t.end();
-      });
-    });
-
-    t.test('json style', function (t) {
-      conmock({
-        handler: issuer.validator,
-        request: {
-          body: { assertion: '{}'},
-          headers: { accept: 'application/json' }
-        }
-      }, function (err, mock) {
-        const errors = _.pluck(mock.body.errors, 'field').sort();
-        t.same(mock.status, 400, 'should be an error');
-        t.same(errors, expect, 'should get back the right errors');
-        t.end();
-      });
-    });
-  });
-
+  
   test('issuer#welcome: no user', function (t) {
     conmock(issuer.welcome, function (err, mock) {
       t.same(mock.fntype, 'redirect');
