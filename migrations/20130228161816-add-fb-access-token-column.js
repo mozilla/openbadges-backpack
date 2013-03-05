9370f8aef0f932c3898c9eee1d3082b0a39d9fa8
Dave Lester <davelester@gmail.com>
2013-03-05 15:22:17 -0800

    Removes unused async code from db migration.

--- migrations/20130228161816-add-fb-access-token-column.js
+++ migrations/20130228161816-add-fb-access-token-column.js
@@ -3,15 +3,11 @@ var async = require('async');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-  async.series([
-    db.runSql.bind(db, 'ALTER TABLE `user` ' +
-                       'ADD fb_access_token VARCHAR(255) DEFAULT NULL')
-  ], callback);
+  db.runSql('ALTER TABLE `user` ' +
+            'ADD fb_access_token VARCHAR(255) DEFAULT NULL', callback);
 };
 
 exports.down = function(db, callback) {
-  async.series([
-    db.runSql.bind(db, 'ALTER TABLE `user` ' +
-                       'DROP COLUMN fb_access_token;')
-  ], callback);
+  db.runSql('ALTER TABLE `user` ' +
+            'DROP COLUMN fb_access_token', callback);
 };
