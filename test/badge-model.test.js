fa71f91011d8eb38f7fe1009e96f6a94430107e1
Brian J Brennan <brianloveswords@gmail.com>
2013-03-05 14:24:58 -0500

    Remove old validation tests.
    I want to start from scratch with the tests so I can get a better
    implementation that caters to the new validator rather than trying to
    shoehorn the new validator into the old validator output.

--- test/badge-model.test.js
+++ test/badge-model.test.js
@@ -27,128 +27,6 @@ testUtils.prepareDatabase({
     })
   });
 
-  test('Badge.validateBody', function (t) {
-    const validate = Badge.validateBody;
-    const $ = testUtils.makeAssertion;
-    const randstr = testUtils.randomstring;
-
-    // Test Helpers
-    function hasError(err, field) {
-      if (!err) return false;
-      if (!err.fields) return false;
-      if (!err.fields[field]) return false;
-      return true;
-    }
-    function isMissing(err, field) {
-      if (!hasError(err, field)) return false;
-      return !!err.fields[field].match(/missing/);
-    }
-    function isInvalid(err, field) {
-      if (!hasError(err, field)) return false;
-      return !!err.fields[field].match(/invalid/);
-    }
-    function isValid(err, field) {
-      return !hasError(err, field);
-    }
-    function getError(field, data) {
-      const changes = {};
-      changes[field] = data;
-      return validate($(changes));
-    }
-    function testMissing(field) {
-      const err = getError(field, null);
-      t.ok(isMissing(err, field), 'missing ' + field);
-    }
-    function testInvalid(field, data) {
-      const err = getError(field, data);
-      t.ok(isInvalid(err, field), 'invalid ' + field + ': '+ data);
-    }
-    function testValid(field, data) {
-      const err = getError(field, data);
-      t.ok(isValid(err, field), 'valid ' + field + ': '+ data);
-    }
-    function testManyInvalid(field, dataArray) {
-      const runTest = testInvalid.bind(null, field);
-      dataArray.map(runTest);
-    }
-    function testManyValid(field, dataArray) {
-      const runTest = testValid.bind(null, field);
-      dataArray.map(runTest);
-    }
-
-    // Valid and invalid data
-    const RECIPIENTS = {
-      good: ['brian@awesome.com', 'yo+wut@example.com', 'ümlaut@heavymetal.de', 'sha1$c0b19425e0f2c8021ab06c79b19144e127b0f2cb', 'sha256$406f04039d10c79c070b26781e8246dc01ed1d0453c5ad0fa705ff7d507fd898'],
-      bad: ['lkajd', 'skj@asdk', '@.com', '909090', '____!@', 'sha1stuff', 'bcrypt$5$something']
-    };
-    const EMAILS = {
-      good: ['brian@awesome.com', 'yo+wut@example.com', 'ümlaut@heavymetal.de'],
-      bad: ['lkajd', 'skj@asdk', '@.com', '909090', '____!@', 'sha1stuff']
-    };
-    const URLS = {
-      good: ['http://example.com/', 'https://example.com/w/yo', '/partial/path', '/rad.awesome/great/', '/foreign/crázy/ååú´¨la/'],
-      bad: ['-not-asdo', 'ftp://bad-scheme', '@.com:90/', 'just totally wrong']
-    };
-    const ORIGINS = {
-      good: ['http://example.com', 'https://example.com:80', 'https://www.example.com', 'https://www.example.com:8080', 'http://example.com/'],
-      bad: ['-not-asdo', 'ftp://bad-scheme', '@.com:90/', 'just totally wrong', 'http://example.com/what', 'http://example.com:8080/false']
-    };
-    const DATES = {
-      good: [Date.now()/1000 | 0, '2012-01-01'],
-      bad: ['oiajsd09gjas;oj09', 'foreever ago', '@.com:90/', '2001-10-190-19', '901d1', '000000000000000000000']
-    };
-    const VERSIONS = {
-      good: ['0.1.1', '2.0.1', '1.2.3', 'v1.2.1'],
-      bad: ['v100', '50', 'v10.1alpha', '1.2.x']
-    };
-    const BADGE_NAME_MAXLENGTH = 128;
-    const BADGE_DESCRIPTION_MAXLENGTH = 128;
-    const ISSUER_NAME_MAXLENGTH = 128;
-    const ISSUER_ORG_MAXLENGTH = 128;
-
-    // Required fields
-    testMissing('recipient');
-    testMissing('badge');
-    testMissing('badge.version');
-    testMissing('badge.name')
-    testMissing('badge.description');
-    testMissing('badge.image');
-    testMissing('badge.criteria');
-    testMissing('badge.issuer');
-    testMissing('badge.issuer.origin');
-    testMissing('badge.issuer.name');
-
-    // Invalid data
-    testManyInvalid('recipient', RECIPIENTS.bad);
-    testManyInvalid('evidence', URLS.bad);
-    testManyInvalid('expires', DATES.bad);
-    testManyInvalid('issued_on', DATES.bad);
-    testInvalid('badge.name', randstr(BADGE_NAME_MAXLENGTH + 1));
-    testInvalid('badge.description', randstr(BADGE_DESCRIPTION_MAXLENGTH + 1));
-    testManyInvalid('badge.image', URLS.bad);
-    testManyInvalid('badge.criteria', URLS.bad);
-    testManyInvalid('badge.issuer.origin', ORIGINS.bad);
-    testInvalid('badge.issuer.name', randstr(ISSUER_NAME_MAXLENGTH + 1));
-    testInvalid('badge.issuer.org', randstr(ISSUER_ORG_MAXLENGTH + 1));
-    testManyInvalid('badge.issuer.contact', EMAILS.bad);
-
-    // Valid data
-    testManyValid('evidence', URLS.good);
-    testManyValid('expires', DATES.good);
-    testManyValid('issued_on', DATES.good);
-    testValid('badge.name', randstr(BADGE_NAME_MAXLENGTH));
-    testValid('badge.description', randstr(BADGE_DESCRIPTION_MAXLENGTH));
-    testManyValid('badge.image', URLS.good);
-    testManyValid('badge.criteria', URLS.good);
-    testManyValid('badge.issuer.origin', ORIGINS.good);
-    testValid('badge.issuer.name', randstr(ISSUER_NAME_MAXLENGTH));
-    testValid('badge.issuer.org', randstr(ISSUER_ORG_MAXLENGTH));
-    testManyValid('badge.issuer.contact', EMAILS.good);
-
-    t.same(validate($()), null, 'valid data should be valid');
-    t.end();
-  });
-
   test('Badge#save: sets the `body_hash` correctly', function (t) {
     const SHA256_LENGTH = 64;
     const expect = SHA256_LENGTH;
@@ -223,8 +101,6 @@ testUtils.prepareDatabase({
     err = validate({body: "Sludge Metal"});
     t.ok(hasError(err, 'body'), 'body error on invalid type');
 
-    err = validate({body: testUtils.makeAssertion({badge: null})});
-    t.ok(hasError(err, 'body'), 'body error on invalid assertion');
     t.end();
   });
 
