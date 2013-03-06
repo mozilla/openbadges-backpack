3499f338ce8e05b8972cde02ad1918ca8ed93dad
Brian J Brennan <brianloveswords@gmail.com>
2013-03-06 16:31:11 -0500

    Migration for `image_data` field.
    We need to stop storing badge images on the disk. This is the first step
    to doing just that. It creates a new column in the `badge` table and
    tries to populate it with the contents of the files pointed to in the
    `image_path` column.

--- test/migration.test.js
+++ test/migration.test.js
@@ -159,3 +159,25 @@ testMigration("drop-rejected-column-from-badge", function(t, id, previousId) {
   ];
 });
 
+testMigration("add-image-data-column", function(t, id, previousId) {
+  const fs = require('fs');
+  const conf = require('../lib/configuration')
+  const image1 = fs.readFileSync(__dirname + '/data/static/_badges/image1.png');
+  const image2 = fs.readFileSync(__dirname + '/data/static/_badges/image2.png');
+  conf.set('badge_path', __dirname + '/data/static/_badges');
+
+  return [
+    up({destination: previousId}),
+    sql("INSERT INTO `user` VALUES (1,'foo@bar.org',NULL,1,NULL,NULL);"),
+    sql("INSERT INTO `badge` (id, user_id, image_path, body, body_hash) VALUES (1,1, '/_badges/image1.png','body','hsh1')"),
+    sql("INSERT INTO `badge` (id, user_id, image_path, body, body_hash) VALUES (2,1, '/_badges/image2.png','body','hsh2')"),
+    up({count: 1}),
+    sql("SELECT image_data FROM badge ORDER BY `id` ASC", function(results) {
+      t.same(Buffer(results[0].image_data, 'base64'), image1);
+      t.same(Buffer(results[1].image_data, 'base64'), image2);
+    }),
+    down({count: 1}),
+    sqlError("SELECT image_data FROM badge", t, "ERROR_BAD_FIELD_ERROR"),
+  ];
+});
+
