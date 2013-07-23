edb5685c2aa8e8178476f3f2200347efc97fc2c9
Andrew Sliwinski <thisandagain@gmail.com>
2013-07-22 20:15:50 -0700

    Isolate to /backpack/badge POST

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ app.get('/backpack/add', backpack.addBadge);
 app.get('/backpack/login', backpack.login);
 app.get('/backpack/signout', backpack.signout);
 app.post('/backpack/badge', middleware.statsdRequests(), backpack.userBadgeUpload);
-app.post('/backpack/authenticate', middleware.statsdRequests(), backpack.authenticate);
+app.post('/backpack/authenticate', backpack.authenticate);
 app.get('/backpack/settings', backpack.settings());
 app.post('/backpack/settings/revoke-origin', backpackConnect.revokeOrigin());
 app.get('/stats', backpack.stats);
