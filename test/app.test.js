73d514cd6f553f6e98b103b565301f4377003e25
Atul Varma <varmaa@gmail.com>
2013-08-15 11:19:31 -0400

    moved template-related tests from app.test.js to template.test.js.

--- test/app.test.js
+++ test/app.test.js
@@ -29,46 +29,9 @@ describe("app", function() {
       .expect(403, done);
   });
 
-  it('auto-escapes template variables', function(done) {
-    request({
-      testRoutes: {
-        'GET /escaping': function(req, res) {
-          return res.render('escaping.html', {foo: '<script>'});
-        }
-      },
-      testTemplates: {'escaping.html': 'hi {{foo}}'}
-    })
-      .get('/escaping')
-      .expect('hi &lt;script&gt;')
-      .expect(200, done);
-  });
-
-  it('defines DOT_MIN in app.locals', function() {
-    testUtil.app({debug: true}).locals.DOT_MIN
-      .should.equal('');
-    testUtil.app({debug: false}).locals.DOT_MIN
-      .should.equal('.min');
-  });
-
   it('defines PERSONA_JS_URL in app.locals', function() {
     testUtil.app().locals.PERSONA_JS_URL
       .should.match(/persona\.org\/include\.js/);
-  });
-
-  it('defines csrfToken in res.locals', function(done) {
-    request({testRoutes: {
-      'GET /csrf': function(req, res) {
-        res.send(typeof(res.locals.csrfToken) + res.locals.csrfToken.length);
-      }
-    }}).get('/csrf').expect('string24', done);
-  });
-
-  it('defines email in res.locals', function(done) {
-    request({testRoutes: {
-      'GET /email': function(req, res) {
-        res.send(typeof(res.locals.email) + res.locals.email.length);
-      }
-    }}).get('/email').expect('string0', done);
   });
 
   it('defines POST /persona/verify', function() {
--- test/template.test.js
+++ test/template.test.js
@@ -1,7 +1,47 @@
 var _ = require('underscore');
 var should = require('should');
 
-var request = require('./lib/util').request;
+var testUtil = require('./lib/util');
+var request = testUtil.request;
+
+describe("template middleware", function() {
+  it('auto-escapes template variables', function(done) {
+    request({
+      testRoutes: {
+        'GET /escaping': function(req, res) {
+          return res.render('escaping.html', {foo: '<script>'});
+        }
+      },
+      testTemplates: {'escaping.html': 'hi {{foo}}'}
+    })
+      .get('/escaping')
+      .expect('hi &lt;script&gt;')
+      .expect(200, done);
+  });
+
+  it('defines DOT_MIN in app.locals', function() {
+    testUtil.app({debug: true}).locals.DOT_MIN
+      .should.equal('');
+    testUtil.app({debug: false}).locals.DOT_MIN
+      .should.equal('.min');
+  });
+
+  it('defines csrfToken in res.locals', function(done) {
+    request({testRoutes: {
+      'GET /csrf': function(req, res) {
+        res.send(typeof(res.locals.csrfToken) + res.locals.csrfToken.length);
+      }
+    }}).get('/csrf').expect('string24', done);
+  });
+
+  it('defines email in res.locals', function(done) {
+    request({testRoutes: {
+      'GET /email': function(req, res) {
+        res.send(typeof(res.locals.email) + res.locals.email.length);
+      }
+    }}).get('/email').expect('string0', done);
+  });
+});
 
 describe("layout.html", function() {
   function layoutRequest(options) {
