221268c0b24e76114b41c39bd5a63eb983841ca8
Mike Larsson <mikelarssonftw@gmail.com>
2013-09-18 11:23:02 -0400

    Add a simple statsd middleware test

--- test/statsd.test.js
+++ test/statsd.test.js
@@ -1,4 +1,5 @@
 const tap = require('tap').test;
+const sinon = require('sinon');
 const statsd = require(__dirname + '/../lib/statsd.js');
 
 tap('statsd', function (t) {
@@ -8,6 +9,21 @@ tap('statsd', function (t) {
   // Methods
   statsd.increment('test.bucket');
   statsd.decrement('test.bucket');
+
+  t.test('middleware', function (t) {
+    const middleware = require('../middleware').statsdRequests();
+    sinon.spy(statsd, 'increment');
+
+    middleware({ 
+      path: '/some/path',
+      method: 'METHOD'
+    }, {}, function(){
+      t.ok(statsd.increment.calledOnce, 'increment called');
+      t.deepEqual(statsd.increment.args[0], ['paths.some.path.method']);
+      sinon.restore();
+      t.end();
+    });
+  });
 
   // Force purge singleton
   t.end();
