4095008b063b676cf77c5399288c91deb804182b
Keith Hill <keith@bluemantis.com>
2016-11-29 15:20:54 +0000

    Set token refresh to be outside of bearer auth strategy to maintain backwards compatibility

--- routes.js
+++ routes.js
@@ -145,8 +145,8 @@ module.exports = function(app, passport, parseForm, csrfProtection) {
   app.post('/accept', parseForm, csrfProtection, backpackConnect.allowAccess());
 
   // Backpack Connect API
+  app.post('/api/token', parseForm, backpackConnect.refresh()); // excluded from bearer auth
   app.all('/api/*', passport.authenticate('bearer', { session: false }), backpackConnect.allowCors());
-  app.post('/api/token', passport.authenticate('bearer', { session: false }), parseForm, backpackConnect.refresh());
   app.post('/api/issue', passport.authenticate('bearer', { session: false }), parseForm, backpackConnect.authorize("issue"),
                          issuer.issuerBadgeAddFromAssertion);
   app.get('/api/identity', passport.authenticate('bearer', { session: false }), backpackConnect.authorize("issue"),
