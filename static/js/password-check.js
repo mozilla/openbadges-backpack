d73d1238236054b341a6c8499e4b69c865c923fa
Keith Hill <keith@bluemantis.com>
2016-12-08 11:28:51 +0000

    Added live password requirements check to signup and migration

---
+++ static/js/password-check.js
@@ -0,0 +1,42 @@
+$(function() {
+  var requirements = [
+    {
+      'selector': 'req-1',
+      'rule': 'The password must be at least 10 characters long.'
+    },
+    {
+      'selector': 'req-2',
+      'rule': 'The password must contain at least one uppercase letter.'
+    },
+    {
+      'selector': 'req-3',
+      'rule': 'The password must contain at least one number.'
+    },
+    {
+      'selector': 'req-4',
+      'rule': 'The password must contain at least one special character.'
+    },
+    {
+      'selector': 'req-5',
+      'rule': 'The password may not contain sequences of three or more repeated characters.'
+    },
+  ];
+
+  $('input[type="password"]').keyup(function() {
+    var results = owaspPasswordStrengthTest.test($(this).val());
+    for (var i = requirements.length - 1; i >= 0; i--) {
+      if (results.errors.indexOf(requirements[i].rule) === -1) {
+        $('.' + requirements[i].selector).addClass('met');
+      } else {
+        $('.' + requirements[i].selector).removeClass('met');
+      }
+      if (i == (requirements.length - 1)) {
+        if (results.errors.length == 0) {
+          $('input[type="submit"]').prop('disabled', false);
+        } else {
+          $('input[type="submit"]').prop('disabled', true);
+        }
+      }
+    }
+  });
+});
